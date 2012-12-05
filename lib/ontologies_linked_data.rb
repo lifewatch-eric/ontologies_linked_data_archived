f20f93ff29f56adc268a3b0a1c1492e930a58cd8
Paul R Alexander <palexander@stanford.edu>
2012-12-05 12:18:06 -0800

    Fix new require locations

--- lib/ontologies_linked_data.rb
+++ lib/ontologies_linked_data.rb
@@ -1,10 +1,10 @@
 require "goo"
-require_relative "lib/serializer"
-require_relative "lib/monkeypatches/to_flex_hash/object"
+require_relative "ontologies_linked_data/serializer"
+require_relative "ontologies_linked_data/monkeypatches/to_flex_hash/object"
 
 # Require all models
 project_root = File.dirname(File.absolute_path(__FILE__))
-Dir.glob(project_root + '/lib/models/*', &method(:require))
+Dir.glob(project_root + '/models/*', &method(:require))
 
 # Setup Goo (repo connection and namespaces)
 module LinkedData
