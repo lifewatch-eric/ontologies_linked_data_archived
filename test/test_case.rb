b135fb49db8383111ac778f9371d0e7119d47c29
Paul R Alexander <palexander@stanford.edu>
2012-12-05 13:00:09 -0800

    Fix test case locations

--- test/test_case.rb
+++ test/test_case.rb
@@ -1,8 +1,8 @@
 require_relative "../config/default.rb"
-require_relative "../ontologies_linked_data"
+require_relative "../lib/ontologies_linked_data"
 require "test/unit"
 
-LinkedData.config(port: GOO_PORT, host: GOO_HOST)
+LinkedData.config
 
 module LinkedData
   class TestCase < Test::Unit::TestCase
