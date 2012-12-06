ed3901f9a780dd485e2205ca867216499adeb767
Paul R Alexander <palexander@stanford.edu>
2012-12-06 12:46:30 -0800

    Better ontology tests

--- test/models/test_ontology.rb
+++ test/models/test_ontology.rb
@@ -1,24 +1,44 @@
 require_relative "../test_case"
 
 class TestOntology < LinkedData::TestCase
+  def setup
+    @acronym = "SNOMED-TST"
+    @name = "SNOMED-CT TEST"
+  end
+
+  def teardown
+    o = LinkedData::Models::Ontology.find(@acronym)
+    o.delete unless o.nil?
+  end
+
   def test_valid_ontology
     o = LinkedData::Models::Ontology.new
     assert (not o.valid?)
 
-    o.acronym = "SNOMED"
-    o.name = "SNOMED CT"
+    o.acronym = @acronym
+    o.name = @name
     assert o.valid?
   end
 
-  def test_ontology_save
+  def test_ontology_lifecycle
     o = LinkedData::Models::Ontology.new({
-        acronym: "NEW_ONT",
-        name: "Test Ontology"
+        acronym: @acronym,
+        name: @name
       })
 
+    # Create
     assert_equal false, o.exist?(reload=true)
     o.save
     assert_equal true, o.exist?(reload=true)
+
+    # Update
+    new_name = "New Name"
+    o.name = new_name
+    o.save
+    o1 = LinkedData::Models::Ontology.find(@acronym)
+    assert o1.name.eql?(new_name)
+
+    # Delete
     o.delete
     assert_equal false, o.exist?(reload=true)
   end
