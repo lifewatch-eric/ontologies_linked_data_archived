3f5976fb4549e4de7b558a390e5d5c688e41dc17
msalvadores <msalvadores@gmail.com>
2012-12-07 17:21:27 -0800

    Changes in test ontology to not touch the "name" attribute that does not exist in ontology anymore.

--- lib/ontologies_linked_data/models/ontology.rb
+++ lib/ontologies_linked_data/models/ontology.rb
@@ -2,8 +2,12 @@ module LinkedData
   module Models
     class Ontology < Goo::Base::Resource
       model :ontology
-      attribute :acronym, :unique => true, :cardinality => { :max => 1, :min => 1 }
-      attribute :name, :cardinality => { :max => 1, :min => 1 }
+      attribute :acronym, :unique => true
+
+      #TODO not yet supported in goo
+      #https://github.com/ncbo/goo/issues/32
+      #attribute :submissions, :inverse_of { :with => :ontology_submission , :attribute => :ontology }
+
     end
   end
 end
