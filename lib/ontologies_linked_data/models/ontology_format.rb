8a7f08a03f87894eca2ec726e4d0261377419029
msalvadores <msalvadores@gmail.com>
2012-12-10 12:24:32 -0800

    OntologyFormat model

---
+++ lib/ontologies_linked_data/models/ontology_format.rb
@@ -0,0 +1,17 @@
+module LinkedData
+  module Models
+    class OntologyFormat < Goo::Base::Resource
+      model :ontology_format
+      attribute :acronym, :unique => true
+      
+      def self.init
+        ["OBO", "OWL"].each do |acr|
+          of =  LinkedData::Models::OntologyFormat.new( { :acronym => acr } )
+          if not of.exist?
+            of.save
+          end
+        end
+      end
+    end
+  end
+end
