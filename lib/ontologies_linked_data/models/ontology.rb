7a948d578b61d33bd1075b405174fe75aafc4889
Paul R Alexander <palexander@stanford.edu>
2012-12-20 13:50:59 -0800

    Method for getting next available submission id

--- lib/ontologies_linked_data/models/ontology.rb
+++ lib/ontologies_linked_data/models/ontology.rb
@@ -8,7 +8,21 @@ module LinkedData
                   :inverse_of => { :with => :ontology_submission,
                   :attribute => :ontology }
 
+      def next_submission_id
+        submissions = OntologySubmission.where(acronym: self.acronym)
 
+        # This is the first!
+        return 1 if submissions.nil? || submissions.empty?
+
+        # Try to get a new one based on the old
+        submission_ids = []
+        submissions.each do |s|
+          s.load unless s.loaded?
+          submission_ids << s.submissionId.to_i
+        end
+        return submission_ids.max + 1
+      end
+      end
     end
   end
 end
