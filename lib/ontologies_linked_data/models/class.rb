cd02051517cf674e6a6f35122f374ed90b575bbf
msalvadores <msalvadores@gmail.com>
2013-01-09 17:45:34 -0800

    Included retrieval of synonyms in class. Useful for label generation in parsing.

--- lib/ontologies_linked_data/models/class.rb
+++ lib/ontologies_linked_data/models/class.rb
@@ -6,38 +6,50 @@ module LinkedData
       attr_accessor :id
       attr_accessor :graph
 
-      def initialize(id,graph,prefLabel = nil)
+      def initialize(id,graph,prefLabel = nil, synonymLabel = nil)
         @id = id
         @prefLabel = prefLabel
+        @synonymLabel = synonymLabel
         @graph = graph
       end
 
       def prefLabel
         return (@prefLabel ? @prefLabel.value : nil)
-      end 
+      end
+
+      def synonymLabel
+        @synonymLabel.select!{ |sy| sy != nil }
+        return (@synonymLabel ? (@synonymLabel.map { |sy| sy.value })  : [])
+      end
 
       def self.where(*args)
         if args.length == 1 and args[0].include? :graph
           params = args[0]
           graph = params[:graph]
-          prefLabelProperty =  params[:prefLabelProperty] || LinkedData::Utils::Namespaces.default_pref_label 
+          prefLabelProperty =  params[:prefLabelProperty] || LinkedData::Utils::Namespaces.default_pref_label
           classType =  params[:classType] || LinkedData::Utils::Namespaces.default_type_for_classes
 
           query = <<eos
-SELECT DISTINCT ?id ?label WHERE {
+SELECT DISTINCT ?id ?prefLabel ?synonymLabel WHERE {
   GRAPH <#{graph.value}> {
     ?id a <#{classType.value}> .
-    OPTIONAL { ?id <#{LinkedData::Utils::Namespaces.skos_prefLabel}> ?label . }
-} }
+    OPTIONAL { ?id <#{LinkedData::Utils::Namespaces.default_pref_label.value}> ?prefLabel . }
+    OPTIONAL { ?id <#{LinkedData::Utils::Namespaces.rdfs_label}> ?synonymLabel . }
+    FILTER(!isBLANK(?id))
+} } ORDER BY ?id
 eos
           rs = Goo.store.query(query)
           classes = []
           rs.each_solution do |sol|
-            classes << Class.new(sol.get(:id),graph, sol.get(:label))
+            if ((classes.length > 0) and (classes[-1].id.value == sol.get(:id).value))
+              classes[-1].synonymLabel << sol.get(:synonymLabel)
+            else
+              classes << Class.new(sol.get(:id),graph, sol.get(:prefLabel), [sol.get(:synonymLabel)])
+            end
           end
           return classes
         else
-          raise ArgumentError, "Current Class implementation search capabilities are minimal" 
+          raise ArgumentError, "Current Class implementation search capabilities are minimal"
         end
       end
 
