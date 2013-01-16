2f3394e181161e635b0030750e48e6ae3179ac17
Paul R Alexander <palexander@stanford.edu>
2013-01-16 12:02:05 -0800

    Setup Goo automatically

--- lib/ontologies_linked_data.rb
+++ lib/ontologies_linked_data.rb
@@ -7,6 +7,24 @@ require_relative "ontologies_linked_data/utils/namespaces"
 require_relative "ontologies_linked_data/parser/parser"
 require_relative "ontologies_linked_data/monkeypatches/to_flex_hash/object"
 
+# Setup Goo (repo connection and namespaces)
+port = $GOO_PORT || 9000
+host = $GOO_HOST || "localhost"
+begin
+  if Goo.store().nil?
+    Goo.configure do |conf|
+      conf[:stores] = [ { :name => :main , :host => host, :port => port, :options => { } } ]
+      conf[:namespaces] = {
+        :metadata => "http://data.bioontology.org/metadata/",
+        :omv => "http://omv.org/ontology/",
+        :default => :metadata
+      }
+    end
+  end
+rescue Exception => e
+  puts "Invalid triplestore configuration, moving on"
+end
+
 # Require base model
 require_relative "ontologies_linked_data/models/base"
 
@@ -14,23 +32,3 @@ require_relative "ontologies_linked_data/models/base"
 project_root = File.dirname(File.absolute_path(__FILE__))
 Dir.glob(project_root + '/ontologies_linked_data/models/*', &method(:require))
 
-# Setup Goo (repo connection and namespaces)
-module LinkedData
-  def self.config(options = {})
-    port = options[:port] || $GOO_PORT || 9000
-    host = options[:host] || $GOO_HOST || "localhost"
-    begin
-      if Goo.store().nil?
-        Goo.configure do |conf|
-          conf[:stores] = [ { :name => :main , :host => host, :port => port, :options => { } } ]
-          conf[:namespaces] = {
-            :metadata => "http://data.bioontology.org/metadata/",
-            :default => :metadata,
-          }
-        end
-      end
-    rescue Exception => e
-      puts "Invalid triplestore configuration, moving on"
-    end
-  end
-end
