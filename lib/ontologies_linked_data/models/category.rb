2682fb4fd4655fbe16166f32717ff1d4d6c83d01
msalvadores <msalvadores@gmail.com>
2013-01-16 17:29:34 -0800

    Changes to the category model

--- lib/ontologies_linked_data/models/category.rb
+++ lib/ontologies_linked_data/models/category.rb
@@ -1,6 +1,9 @@
 module LinkedData
   module Models
     class Category < LinkedData::Models::Base
+      model :category
+      attribute :name, :unique => true, :not_nil => true
+      attribute :created, :single_value => true, :default => lambda { |o| DateTime.now }
     end
   end
 end
