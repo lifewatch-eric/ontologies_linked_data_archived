5cf64216020f7544900c6b8f7f4c7a09f92f4bf1
Michael Dorf <mdorf@stanford.edu>
2013-01-17 16:07:51 -0800

    Proper checking for custom.rb

--- config/default.rb
+++ config/default.rb
@@ -4,4 +4,5 @@ $GOO_HOST = "localhost"
 $REPOSITORY_FOLDER = "./test/data/ontology_files/repo"
 
 # Settings in this file can be overridden in the custom.rb (warnings may happen)
-require_relative "custom.rb" if File.exists?("custom.rb")
+custom = File.expand_path('../custom.rb', __FILE__)
+require_relative "custom.rb" if File.exists?(custom)
