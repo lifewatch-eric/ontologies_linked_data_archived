1b071287a834c42962ac16c6c73dcee9599e6d9f
Paul R Alexander <palexander@stanford.edu>
2013-01-18 18:04:17 -0800

    Revert "Category clean for tutorial."
    This reverts commit 8e1e02badb6e90117efa981862013f03e7499c74.

--- lib/ontologies_linked_data/models/category.rb
+++ lib/ontologies_linked_data/models/category.rb
@@ -1,6 +1,12 @@
 module LinkedData
   module Models
     class Category < LinkedData::Models::Base
+      model :category
+      attribute :created, :date_time_xsd => true, :cardinality => { :max => 1, :min => 1 }
+      attribute :name, :cardinality => { :max => 1, :min => 1 }
+      attribute :acronym, :unique => true, :cardinality => { :max => 1, :min => 1 }
+      attribute :description, :cardinality => { :max => 1, :min => 1 }
+      attribute :parentCategory, :instance_of => { :with => :cateogory }
     end
   end
 end
