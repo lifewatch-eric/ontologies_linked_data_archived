f4c52c6eb995a849cc3a804c6c76cd6f9d8775e2
Paul R Alexander <palexander@stanford.edu>
2013-01-23 13:10:31 -0800

    New user attributes

--- lib/ontologies_linked_data/models/user.rb
+++ lib/ontologies_linked_data/models/user.rb
@@ -3,6 +3,9 @@ module LinkedData
     class User < LinkedData::Models::Base
       model :user
       attribute :username, :unique => true, :cardinality => { :max => 1, :min => 1 }
+      attribute :firstName, :single_value => true
+      attribute :lastName, :single_value => true
+      attribute :created, :date_time_xsd => true, :single_value => true
     end
   end
 end
