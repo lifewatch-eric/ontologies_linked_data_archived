8dcb4d9fc3925a6bf44e35edf2aa15b25641b34e
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-01-24 13:03:09 -0800

    Update project model and tests; added unique property to the name attribute and default DateTime value for created attribute.

--- lib/ontologies_linked_data/models/project.rb
+++ lib/ontologies_linked_data/models/project.rb
@@ -2,13 +2,16 @@ module LinkedData
   module Models
     class Project < LinkedData::Models::Base
       model :project
-      attribute :creator, :cardinality => { :max => 1, :min => 1 }
-      attribute :created, :date_time_xsd => true, :cardinality => { :max => 1, :min => 1 }
-      attribute :name, :cardinality => { :max => 1, :min => 1 }
-      attribute :homePage, :cardinality => { :max => 1, :min => 1 }
-      attribute :description, :cardinality => { :max => 1, :min => 1 }
+      #, :name_with =>
+      attribute :creator, :instance_of => { :with => :user }, :cardinality => { :min => 1, :max => 1 }
+      attribute :created, :date_time_xsd => true, :cardinality => { :max => 1 }, :default => lambda {|x| DateTime.new }
+      attribute :name, :unique => true, :cardinality => { :min => 1, :max => 1 }
+      #attribute :homePage, :instance_of => { :with => URI }, :cardinality => { :min => 1, :max => 1 }
+      attribute :homePage, :cardinality => { :min => 1, :max => 1 }
+      attribute :description, :cardinality =>  { :min => 1, :max => 1 }
       attribute :contacts, :cardinality => { :max => 1 }
       attribute :ontologyUsed, :instance_of => { :with => :ontology }, :cardinality => { :min => 1 }
     end
   end
 end
+
