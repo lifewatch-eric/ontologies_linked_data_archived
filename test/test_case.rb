ba05aa0f64ebdb81ec50beb54776a3656b2adcac
Paul R Alexander <palexander@stanford.edu>
2013-01-25 18:17:37 -0800

    Add methods to generate ontologies

--- test/test_case.rb
+++ test/test_case.rb
@@ -53,5 +53,86 @@ module LinkedData
       #Submission Status
       return owl, ont, user, status
     end
+
+    def teardown
+      delete_ontologies_and_submissions
+    end
+
+    ##
+    # Creates a set of Ontology and OntologySubmission objects and stores them in the triplestore
+    # @param [Hash] options the options to create ontologies with
+    # @option options [Fixnum] :ont_count Number of ontologies to create
+    # @option options [Fixnum] :submission_count How many submissions each ontology should have (acts as max number when random submission count is used)
+    # @option options [TrueClass, FalseClass] :random_submission_count Use a random number of submissions between 1 and :submission_count
+    def create_ontologies_and_submissions(options = {})
+      ont_count = options[:ont_count] || 5
+      submission_count = options[:submission_count] || 5
+      random_submission_count = options[:random_submission_count].nil? ? true : options[:random_submission_count]
+
+      u = LinkedData::Models::User.new(username: "tim", email: "tim@example.org")
+      u.save unless u.exist? || !u.valid?
+
+      of = LinkedData::Models::OntologyFormat.new(acronym: "OWL")
+      of.save unless u.exist? || !u.valid?
+
+      ont_acronyms = []
+      ontologies = []
+      ont_count.to_i.times do |count|
+        acronym = "TST-ONT-#{count}"
+        ont_acronyms << acronym
+
+        o = LinkedData::Models::Ontology.new({
+          acronym: acronym,
+          name: "Test Ontology ##{count}",
+          administeredBy: u
+        })
+
+        if o.valid?
+          o.save
+          ontologies << o
+        end
+
+        # Random submissions (between 1 and max)
+        max = random_submission_count ? (1..submission_count.to_i).to_a.shuffle.first : submission_count
+        max.times do
+          os = LinkedData::Models::OntologySubmission.new({
+            acronym: "TST-ONT-#{count}",
+            ontology: o,
+            hasOntologyLanguage: of,
+            pullLocation: RDF::IRI.new("http://example.com"),
+            submissionStatus: LinkedData::Models::SubmissionStatus.new(:code => "UPLOADED"),
+            submissionId: o.next_submission_id
+          })
+          os.save if os.valid?
+        end
+      end
+
+      # Get ontology objects if empty
+      if ontologies.empty?
+        ont_acronyms.each do |ont_id|
+          ontologies << LinkedData::Models::Ontology.find(ont_id)
+        end
+      end
+
+      return ont_count, ont_acronyms, ontologies
+    end
+
+    ##
+    # Delete all ontologies and their submissions. This will look for all ontologies starting with TST-ONT- and ending in a Fixnum
+    def delete_ontologies_and_submissions
+      ont = LinkedData::Models::Ontology.find("TST-ONT-0")
+      count = 0
+      while ont
+        ont.delete
+        ont = LinkedData::Models::Ontology.find("TST-ONT-#{count+1}")
+      end
+
+      u = LinkedData::Models::User.find("tim")
+      u.delete unless u.nil?
+
+      of = LinkedData::Models::OntologyFormat.find("OWL")
+      of.delete unless u.nil?
+    end
+
   end
 end
