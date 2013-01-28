eaf311c5648c2a1960895c9cec2bab884378b78a
Paul R Alexander <palexander@stanford.edu>
2013-01-28 11:38:52 -0800

    Submissions method comes from goo

--- lib/ontologies_linked_data/models/ontology.rb
+++ lib/ontologies_linked_data/models/ontology.rb
@@ -30,11 +30,6 @@ module LinkedData
         OntologySubmission.where(acronym: acronym, submissionId: submission_id.to_i).first
       end
 
-      def submissions
-        self.load unless self.loaded? || self.attr_loaded?(:acronym)
-        OntologySubmission.where(acronym: acronym)
-      end
-
       def next_submission_id
         (highest_submission_id || 0) + 1
       end
