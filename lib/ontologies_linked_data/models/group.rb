47d1b7c46746fce00dce73d20902a7a67b248e02
Michael Dorf <mdorf@stanford.edu>
2013-01-30 16:47:17 -0800

    added ontologies property to group model

--- lib/ontologies_linked_data/models/group.rb
+++ lib/ontologies_linked_data/models/group.rb
@@ -5,6 +5,7 @@ module LinkedData
       attribute :name, :single_value => true, :not_nil => true
       attribute :description, :single_value => true, :not_nil => true
       attribute :created, :date_time_xsd => true, :single_value => true, :not_nil => true, :default => lambda { |record| DateTime.now }
+      attribute :ontologies, :inverse_of => { :with => :ontology, :attribute => :group }
     end
   end
 end
