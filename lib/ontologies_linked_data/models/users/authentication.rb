82106759105f1ba1d33e3805a91ddef1b8dfbab1
Paul R Alexander <palexander@stanford.edu>
2013-02-05 18:47:23 -0800

    Re-create old system's Java encryption method

--- lib/ontologies_linked_data/models/users/authentication.rb
+++ lib/ontologies_linked_data/models/users/authentication.rb
@@ -9,6 +9,37 @@ module LinkedData
     module Users
       module Authentication
 
+        def valid_legacy_password?(pass, hash)
+          return false if pass.nil? || hash.nil?
+
+          # Work with bytes (8-bit signed integers to match Java)
+          pass = pass.unpack('c*')
+
+          # Decoding the hash using base64 and unpacking the bytes to 8-bit signed integers to match Java
+          old_pass = Base64.decode64(hash).unpack('c*')
+
+          # salt is the first 16 bytes of the old decoded hash
+          salt = old_pass[0..15]
+
+          sha256 = OpenSSL::Digest::SHA256.new
+          sha256.update(salt.pack('c*'))
+          sha256.update(pass.pack('c*'))
+          digest = sha256.digest
+
+          # 100000 iterations where we keep digesting the encrypted (follows Java default)
+          iterations = 100000
+
+          # Java method removes one iteration, not sure why
+          (iterations - 1).times do
+            sha256.reset
+            digest = sha256.digest(digest)
+          end
+
+          # Combine two arrays with the undigested salt plus the digested salt+password
+          encrypted_pass = (salt + digest.unpack('c*'))
+          old_pass == encrypted_pass
+        end
+
       end
     end
   end
