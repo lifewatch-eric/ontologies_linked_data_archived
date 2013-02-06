f7734b73260b69c485a27ab57c4e9e8df36056fb
Paul R Alexander <palexander@stanford.edu>
2013-02-05 18:46:53 -0800

    Authentication module (mixin for users)

---
+++ lib/ontologies_linked_data/models/users/authentication.rb
@@ -0,0 +1,15 @@
+require 'gdbm'
+require 'bcrypt'
+require 'digest'
+require 'openssl'
+require 'base64'
+
+module LinkedData
+  module Models
+    module Users
+      module Authentication
+
+      end
+    end
+  end
+end
