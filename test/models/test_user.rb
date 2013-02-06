afa167429b473c609f464396c6ca677fe8273482
Paul R Alexander <palexander@stanford.edu>
2013-02-05 18:48:30 -0800

    Tests for user authentication

--- test/models/test_user.rb
+++ test/models/test_user.rb
@@ -31,6 +31,61 @@ class TestUser < LinkedData::TestCase
     assert_equal false, u.exist?(reload=true)
   end
 
+  def test_auto_password_hash_assignment
+    password = "a_password"
+    u = LinkedData::Models::User.new({
+        username: "test_user",
+        email: "test@example.com",
+        password: password
+      })
+    assert u.password.nil?
+    assert !u.passwordHash.eql?(password)
+  end
+
+  def test_hash_storage
+    password = "a_password"
+    u = LinkedData::Models::User.new({
+        username: "test_user",
+        email: "test@example.com",
+        password: password
+      })
+    hash = u.passwordHash
+    assert u.valid?
+    u.save
+
+    u1 = LinkedData::Models::User.find("test_user")
+    assert hash.eql?(u1.passwordHash)
+  end
+
+  def test_update_hash
+    # SHA256 hash and password from old Java system
+    hash = "Pa7ZL/klD1M681Hx+0kZrjVwx5Eq9PsoAtII7XR+xWR0p90lgOE37hzXx1u7WiFF"
+    pass = "123456789123456789123456789"
+
+    u = LinkedData::Models::User.new({
+        username: "test_user",
+        email: "test@example.com"
+      })
+
+    u.attributes[:passwordHash] = hash
+    assert u.valid?
+    u.save
+
+    assert u.passwordHash.eql?(hash)
+    assert (not u.valid_password?(pass, u.passwordHash))
+    assert u.authenticate(pass)
+    assert u.valid_password?(pass, u.passwordHash)
+  end
+
+  def test_hash_not_assignable
+    skip("Waiting for read-only attribute support...")
+    u = LinkedData::Models::User.new
+    assert_raise Exception do
+      binding.pry
+      u.passwordHash = "password"
+    end
+  end
+
   def test_user_role_assign
     u = LinkedData::Models::User.new({
         username: "test_user",
