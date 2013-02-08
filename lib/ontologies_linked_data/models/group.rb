5f1c7216d9afbdf009eb0a49dbd85a9e9389c9d7
Paul R Alexander <palexander@stanford.edu>
2013-02-07 20:14:06 -0800

    Description not required

--- lib/ontologies_linked_data/models/group.rb
+++ lib/ontologies_linked_data/models/group.rb
@@ -3,7 +3,7 @@ module LinkedData
     class Group < LinkedData::Models::Base
       attribute :acronym, :unique => true, :single_value => true, :not_nil => true
       attribute :name, :single_value => true, :not_nil => true
-      attribute :description, :single_value => true, :not_nil => true
+      attribute :description
       attribute :created, :date_time_xsd => true, :single_value => true, :not_nil => true, :default => lambda { |record| DateTime.now }
       attribute :ontologies, :inverse_of => { :with => :ontology, :attribute => :group }
     end
