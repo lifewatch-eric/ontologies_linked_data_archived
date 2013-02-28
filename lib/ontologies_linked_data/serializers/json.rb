f6e30f2feeafda7a87e0f695437e44dded385e71
Paul R Alexander <palexander@stanford.edu>
2013-02-27 16:13:44 -0800

    Option to not serialize the context

--- lib/ontologies_linked_data/serializers/json.rb
+++ lib/ontologies_linked_data/serializers/json.rb
@@ -3,10 +3,12 @@ module LinkedData
     class JSON
       def self.serialize(obj, options = {})
         hash = obj.to_flex_hash(options) do |hash, obj|
-          hash["@id"] = obj.resource_id.value if obj.respond_to?(:resource_id) && !obj.resource_id.bnode?
+          hash["@id"] = obj.resource_id.value if obj.is_a?(Goo::Base::Resource) && !obj.resource_id.bnode?
           hash["@type"] = obj.class.type_uri if hash["@id"] && obj.class.respond_to?(:type_uri)
-          context = obj.is_a?(Goo::Base::Resource) ? generate_context(obj) : {}
-          hash.merge!(context)
+          if options[:params] && !options[:params]["no_context"].eql?("true") && obj.is_a?(Goo::Base::Resource)
+            context = generate_context(obj)
+            hash.merge!(context)
+          end
         end
         hash.to_json
       end
@@ -28,7 +30,7 @@ module LinkedData
 
           if linked_model && !object.resource_id.bnode?
             # linked object
-            predicate = {"@id" => linked_model.type_uri, "@type" => "@id"} rescue binding.pry
+            predicate = {"@id" => linked_model.type_uri, "@type" => "@id"}
           elsif settings[:namespace]
             # predicate with custom namespace
             predicate = "#{Goo.namespaces[settings[:namespace]]}#{attr}"
