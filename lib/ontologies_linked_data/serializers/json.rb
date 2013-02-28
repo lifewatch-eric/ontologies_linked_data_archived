c10a79486354433655a55ce6d6f54462f5c2f6b0
Paul R Alexander <palexander@stanford.edu>
2013-02-27 16:06:30 -0800

    Move json and jsonp serializers

---
+++ lib/ontologies_linked_data/serializers/json.rb
@@ -0,0 +1,47 @@
+module LinkedData
+  module Serializers
+    class JSON
+      def self.serialize(obj, options = {})
+        hash = obj.to_flex_hash(options) do |hash, obj|
+          hash["@id"] = obj.resource_id.value if obj.respond_to?(:resource_id) && !obj.resource_id.bnode?
+          hash["@type"] = obj.class.type_uri if hash["@id"] && obj.class.respond_to?(:type_uri)
+          context = obj.is_a?(Goo::Base::Resource) ? generate_context(obj) : {}
+          hash.merge!(context)
+        end
+        hash.to_json
+      end
+
+      private
+
+      def self.generate_context(object)
+        return {} if object.resource_id.bnode?
+        hash = {}
+        class_attributes = object.class.goop_settings[:attributes]
+        hash["@vocab"] = "#{Goo.namespaces[Goo.namespaces[:default]]}"
+        class_attributes.each do |attr, settings|
+          if settings && settings[:validators] && settings[:validators][:instance_of]
+            linked_model = settings[:validators][:instance_of][:with]
+            unless linked_model.is_a?(Class)
+              linked_model = Goo.find_model_by_name(settings[:validators][:instance_of][:with])
+            end
+          end
+
+          if linked_model && !object.resource_id.bnode?
+            # linked object
+            predicate = {"@id" => linked_model.type_uri, "@type" => "@id"} rescue binding.pry
+          elsif settings[:namespace]
+            # predicate with custom namespace
+            predicate = "#{Goo.namespaces[settings[:namespace]]}#{attr}"
+          else
+            # predicate with default namespace
+            predicate = nil
+          end
+          hash[attr] = predicate unless predicate.nil?
+        end
+        {"@context" => hash}
+      end
+
+    end
+  end
+end
+
