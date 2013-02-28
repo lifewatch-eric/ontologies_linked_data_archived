ebd08e6e7be00e869b984e0e9ee07c9802a39076
msalvadores <msalvadores@gmail.com>
2013-02-27 18:50:39 -0800

    correct administerBy assign in test test_duplicated_file_names and test_sanity_check_single_file_submission

--- test/models/test_ontology_submission.rb
+++ test/models/test_ontology_submission.rb
@@ -70,7 +70,7 @@ class TestOntologySubmission < LinkedData::TestOntologyCommon
     assert_instance_of Array, ont_submision.errors[:ontology]
     assert_instance_of Array, ont_submision.errors[:hasOntologyLanguage]
     ont_submision.hasOntologyLanguage = owl
-    ont_submision.administeredBy = user
+    bro.administeredBy = user
     ont_submision.ontology = bro
     assert ont_submision.valid?
     assert_equal 0, ont_submision.errors.length
@@ -95,7 +95,7 @@ class TestOntologySubmission < LinkedData::TestOntologyCommon
     ont_submision.released = DateTime.now - 4
     ont_submision.uploadFilePath = uploadFilePath
     ont_submision.hasOntologyLanguage = owl
-    ont_submision.administeredBy = user
+    rad.administeredBy = user
     ont_submision.ontology = rad
     ont_submision.submissionStatus = status
     assert (not ont_submision.valid?)
@@ -130,7 +130,7 @@ class TestOntologySubmission < LinkedData::TestOntologyCommon
     ont_submision.contact = contact
     ont_submision.released = DateTime.now - 4
     ont_submision.hasOntologyLanguage = owl
-    ont_submision.administeredBy = user
+    dup.administeredBy = user
     ont_submision.ontology = dup
     assert (!ont_submision.valid?)
     assert_equal 2, ont_submision.errors.length
