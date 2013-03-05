9f968d94da4cc93bd9e44a79a6d4ffcb9cfc14ca
Paul R Alexander <palexander@stanford.edu>
2013-03-04 16:41:09 -0800

    Serializer test fixes

--- test/serializer/test_serializer_json.rb
+++ test/serializer/test_serializer_json.rb
@@ -2,32 +2,43 @@ require "test/unit"
 require "json"
 require_relative "../../lib/ontologies_linked_data"
 
-class Car < LinkedData::Models::Base
-  attribute :model, :unique => true
-end
-
-class House < LinkedData::Models::Base
-  attribute :location, :unique => true
-end
-
-class Person < LinkedData::Models::Base
-  attribute :name, :unique => true
-  attribute :age
-  attribute :height
-  attribute :carOwned, :instance_of => {:with => :car}
-  attribute :houseOwned, :instance_of => {:with => House}
-end
-
 class TestSerializerOutput < Test::Unit::TestCase
+  class Car < LinkedData::Models::Base
+    attribute :model, :unique => true
+  end
+
+  class House < LinkedData::Models::Base
+    attribute :location, :unique => true
+  end
+
+  class Person < LinkedData::Models::Base
+    attribute :name, :unique => true
+    attribute :age
+    attribute :height
+    attribute :carOwned, :instance_of => {:with => :car}
+    attribute :houseOwned, :instance_of => {:with => House}
+  end
 
   def setup
     @attrs = {name: "Simon", age: 21}
+    @car_attrs = {model: "Firebird"}
+    @house_attrs = {location: "San Martin"}
     teardown
+    @car = Car.new(@car_attrs)
+    @car.save
+    @house = House.new(@house_attrs)
+    @house.save
     @person = Person.new(@attrs)
+    @person.carOwned = @car
+    @person.houseOwned = @house
     @person.save
   end
 
   def teardown
+    house = House.where(@house_attrs).first rescue nil
+    house.delete unless house.nil?
+    car = Car.where(@car_attrs).first rescue nil
+    car.delete unless car.nil?
     person = Person.where(@attrs).first rescue nil
     person.delete unless person.nil?
   end
