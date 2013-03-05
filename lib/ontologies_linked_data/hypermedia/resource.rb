3d7062e888a0c87d168374d6d6a077a4be6a8696
Paul R Alexander <palexander@stanford.edu>
2013-03-04 19:33:44 -0800

    Link-generating code

--- lib/ontologies_linked_data/hypermedia/resource.rb
+++ lib/ontologies_linked_data/hypermedia/resource.rb
@@ -1,7 +1,10 @@
 module LinkedData
   module Hypermedia
     class Link
-      attr_accessor :path, :type
+      attr_accessor :path, :type, :type_uri
+      def initialize(type, path, type_uri = "")
+        @path = path; @type = type; @type_uri = type_uri
+      end
     end
 
     module Resource
