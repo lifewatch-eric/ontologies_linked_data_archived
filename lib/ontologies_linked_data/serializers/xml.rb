8d42319fad632c4c9786358df2eea139b29408c2
Paul R Alexander <palexander@stanford.edu>
2013-03-08 18:46:51 -0800

    Add self link and rel when available for XML

--- lib/ontologies_linked_data/serializers/xml.rb
+++ lib/ontologies_linked_data/serializers/xml.rb
@@ -40,9 +40,14 @@ module LinkedData
         return {} if !object.is_a?(LinkedData::Hypermedia::Resource) || object.class.hypermedia_settings[:link_to].empty?
         links = object.class.hypermedia_settings[:link_to]
         links_output = ::XML::Node.new("links")
+        self_link = ::XML::Node.new("self")
+        self_link['href'] = object.resource_id.value.gsub("http://data.bioontology.org/metadata", $REST_URL_PREFIX)
+        self_link['rel'] = object.class.type_uri
+        links_output << self_link
         links.each do |link|
           link_xml = ::XML::Node.new(link.type)
           link_xml['href'] = LinkedData::Hypermedia.expand_link(link.path, object)
+          link_xml['rel'] = link.type_uri if link.type_uri
           links_output << link_xml
         end
         return links_output
