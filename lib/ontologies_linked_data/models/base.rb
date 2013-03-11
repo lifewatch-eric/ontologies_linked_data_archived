ce69effbfa437f79222b3d6061fed0fafb3fcc01
Paul R Alexander <palexander@stanford.edu>
2013-03-11 16:50:52 -0700

    Custom method for pluralizing resource id

--- lib/ontologies_linked_data/models/base.rb
+++ lib/ontologies_linked_data/models/base.rb
@@ -1,30 +1,25 @@
 require 'active_support/core_ext/string'
+require 'cgi'
 
 module LinkedData
   module Models
     class Base < Goo::Base::Resource
       include LinkedData::Hypermedia::Resource
 
-      def resource_id
-        id = super
-        hack_resource_id(id)
-      end
-
-      def resource_id=(id)
-        super(hack_resource_id(id))
+      def self.plural_resource_id(object, model_name = nil, naming_attr = nil)
+        model_name ||= (model_name || self.goo_name.to_s).pluralize
+        url_prefix = $REST_URL_PREFIX || self.namespace(:default)
+        RDF::IRI.new("#{url_prefix}#{model_name}/#{unique_value(object, self.goop_settings[:attributes], naming_attr)}")
       end
 
       private
 
-      def hack_resource_id(id)
-        if id.value.match("http://data.bioontology.org/metadata")
-          path = id.value.sub("http://data.bioontology.org/metadata", "")
-          path = path.split("/")
-          path[1] = path[1].pluralize
-          path[0] = $REST_URL_PREFIX
-          id = RDF::IRI.new(path.join("/"))
+      def self.unique_value(object, attributes, naming_attr = nil)
+        unless naming_attr
+          self.goop_settings[:attributes].each {|k,v| naming_attr = k if v[:validators] && v[:validators][:unique]}
         end
-        id
+        value = object.instance_variable_get("@"+naming_attr.to_s) || object.send(naming_attr)
+        CGI.escape(value.to_s)
       end
     end
   end
