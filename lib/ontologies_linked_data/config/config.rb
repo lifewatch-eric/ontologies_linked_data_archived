138a7ecea55607b3498ae509df5abacf35c771e6
Paul R Alexander <palexander@stanford.edu>
2013-03-19 12:00:34 -0700

    Don't config namespaces except at first runtime

--- lib/ontologies_linked_data/config/config.rb
+++ lib/ontologies_linked_data/config/config.rb
@@ -37,18 +37,10 @@ module LinkedData
       puts ">> Connecting to rdf store #{host}:#{port} and search server at #{search_server_url}"
       Goo.configure do |conf|
         conf[:stores] = [ { :name => :main , :host => host, :port => port, :options => { :rules => :NONE} } ]
-        conf[:namespaces] = {
-          :metadata => "http://data.bioontology.org/metadata/",
-          :omv => "http://omv.ontoware.org/2005/05/ontology#",
-          :skos => "http://www.w3.org/2004/02/skos/core#",
-          :owl => "http://www.w3.org/2002/07/owl#",
-          :rdfs => "http://www.w3.org/2000/01/rdf-schema#",
-          :default => :metadata
-        }
         conf[:search_conf] = { :search_server => search_server_url }
       end
     rescue Exception => e
-      abort("EXITING: Cannot connect to triplestore and/or search server '#{e}'")
+      abort("EXITING: Cannot connect to triplestore and/or search server:\n  #{e}")
     end
   end
 
