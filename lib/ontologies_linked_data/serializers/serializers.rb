4aca91cdf24e1dbfeecf18a1b691b80237c40401
Paul R Alexander <palexander@stanford.edu>
2013-03-22 17:50:04 -0700

    Move HTML serializer

--- lib/ontologies_linked_data/serializers/serializers.rb
+++ lib/ontologies_linked_data/serializers/serializers.rb
@@ -2,16 +2,12 @@ require_relative '../media_types'
 require_relative 'xml'
 require_relative 'json'
 require_relative 'jsonp'
+require_relative 'html'
 
 module LinkedData
   module Serializers
     def self.serialize(obj, type, options = {})
       SERIALIZERS[type].serialize(obj, options)
-    end
-
-    class HTML
-      def self.serialize(obj, options)
-      end
     end
 
     class Turtle
