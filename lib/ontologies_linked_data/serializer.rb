9b6586a831d6a5bb2ccdf662874eb355e057d273
Paul R Alexander <palexander@stanford.edu>
2013-03-25 16:11:50 -0700

    Move Serializer methods to class level

--- lib/ontologies_linked_data/serializer.rb
+++ lib/ontologies_linked_data/serializer.rb
@@ -4,32 +4,15 @@ require_relative "serializers/serializers"
 
 module LinkedData
   class Serializer
-    def initialize(app = nil)
-      @app = app
-    end
-
-    def call(env)
-      status, headers, response = @app.call(env)
-      build_response(env, status: status, headers: headers, body: response)
-    end
-
-    def build_response(env, options = {})
+    def self.build_response(env, options = {})
       status = options[:status] || 200
       headers = options[:headers] || {}
       body = options[:body] || ""
       obj = options[:ld_object] || body
 
       params = env["rack.request.query_hash"] || Rack::Utils.parse_query(env["QUERY_STRING"])
-      # Client accept header
-      accept = env['rack-accept.request']
-      # Out of the media types we offer, which would be best?
-      best = LinkedData::MediaTypes.base_type(accept.best_media_type(LinkedData::MediaTypes.all)) unless accept.nil?
-      # Try one other method to get the media type
-      best ||= LinkedData::MediaTypes.base_type(env["HTTP_ACCEPT"])
-      # If user provided a format via query string, override the accept header
-      best = params["format"].to_sym if params["format"]
-      # Default format if none is provided
-      best ||= LinkedData::MediaTypes::DEFAULT
+
+      best = best_response_type(env, params)
 
       # Error out if we don't support the foramt
       unless LinkedData::MediaTypes.supported_base_type?(best)
@@ -56,9 +39,22 @@ module LinkedData
       end
     end
 
+    def self.best_response_type(env, params)
+      # Client accept header
+      accept = env['rack-accept.request']
+      # Out of the media types we offer, which would be best?
+      best = LinkedData::MediaTypes.base_type(accept.best_media_type(LinkedData::MediaTypes.all)) unless accept.nil?
+      # Try one other method to get the media type
+      best ||= LinkedData::MediaTypes.base_type(env["HTTP_ACCEPT"])
+      # If user provided a format via query string, override the accept header
+      best = params["format"].to_sym if params["format"]
+      # Default format if none is provided
+      best ||= LinkedData::MediaTypes::DEFAULT
+    end
+
     private
 
-    def response(options = {})
+    def self.response(options = {})
       status = options[:status] || 200
       headers = options[:headers] || {}
       body = options[:body] || ""
@@ -69,7 +65,7 @@ module LinkedData
       [status, headers, [body]]
     end
 
-    def serialize(type, obj, params, request)
+    def self.serialize(type, obj, params, request)
       only = params["include"] || []
       only = only.split(",") unless only.kind_of?(Array)
       only, all = [], true if only[0].eql?("all")
