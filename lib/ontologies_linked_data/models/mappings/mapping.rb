cbd3ac540d19e6256f4ff7f2f09b46ec82abe382
msalvadores <msalvadores@gmail.com>
2013-04-08 17:58:39 -0700

    instance corrections

--- lib/ontologies_linked_data/models/mappings/mapping.rb
+++ lib/ontologies_linked_data/models/mappings/mapping.rb
@@ -8,7 +8,7 @@ module LinkedData
   module Models
     class Mapping < LinkedData::Models::Base
       model :mapping, :name_with => lambda { |s| mapping_id_generator(s) }
-      attribute :term, :cardinality => { :min => 2 }, :instance_of => { :with => LinkedData::Models::Class }
+      attribute :terms, :cardinality => { :min => 2 }, :instance_of => { :with => LinkedData::Models::Class }
       attribute :occurrence, :instance_of => { :with => LinkedData::Models::Occurrence }
 
       def mapping_id_generator(ins)
