f8bd38badb0c11686d4db853558fd0604554b162
msalvadores <msalvadores@gmail.com>
2013-04-08 17:59:33 -0700

    mapping tests

--- test/models/test_mappings.rb
+++ test/models/test_mappings.rb
@@ -3,6 +3,13 @@ require "logger"
 
 class TestMapping < LinkedData::TestOntologyCommon
   def setup
+    if LinkedData::Models::Mapping.all.length > 100
+      puts "KB with too many mappings to run test. Is this pointing to a TEST KB?"
+      raise Exception, "KB with too many mappings to run test. Is this pointing to a TEST KB?"
+    end
+    LinkedData::Models::Mapping.all do |m|
+      m.delete
+    end
     ontologies_parse()
   end
 
@@ -41,6 +48,7 @@ class TestMapping < LinkedData::TestOntologyCommon
     ont_submision.submissionStatus = status
     assert (ont_submision.valid?)
     ont_submision.save
+
     assert_equal true, ont_submision.exist?(reload=true)
     uploaded = LinkedData::Models::SubmissionStatus.find("UPLOADED")
     uploded_ontologies = uploaded.submissions
@@ -59,7 +67,142 @@ class TestMapping < LinkedData::TestOntologyCommon
     uploaded_ont.process_submission Logger.new(STDOUT)
   end
 
-  def test_mappings()
-    assert 1==1
+  def get_process(name)
+    #just some user
+    user = LinkedData::Models::User.all[0]
+
+    #process
+    ps = LinkedData::Models::MappingProcess.where({:name => name })
+    ps.each do |p| 
+      p.delete
+    end
+    p = LinkedData::Models::MappingProcess.new(:owner => user, :name => name)
+    assert p.valid?
+    p.save
+    ps = LinkedData::Models::MappingProcess.where({:name => name })
+    assert ps.length == 1
+    return ps[0]
+  end
+
+  def test_multiple_mapping()
+
+    process = get_process("LOOMTEST") 
+
+    ont1 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest1" })[0]
+    sub1 = ont1.latest_submission
+    ont2 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest2" })[0]
+    sub2 = ont2.latest_submission
+    ont3 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest3" })[0]
+    sub3 = ont3.latest_submission
+
+    LinkedData::Models::Occurrence.all.each do |occ|
+      occ.delete
+    end
+
+    
+    LinkedData::Models::Mapping.all.each do |occ|
+      occ.delete
+    end
+
+    occ12 = LinkedData::Models::Occurrence.new()
+    occ12.ontologies = [ont1, ont2]
+    occ12.process = process
+    assert occ12.valid?
+    occ12.save
+
+    occ13 = LinkedData::Models::Occurrence.new()
+    occ13.ontologies = [ont1, ont3]
+    occ13.process = process
+    assert occ13.valid?
+    occ13.save
+
+    occ23 = LinkedData::Models::Occurrence.new()
+    occ23.ontologies = [ont2, ont3]
+    occ23.process = process
+    assert occ23.valid?
+    occ23.save
+
+    ont1_terms = ["http://bioontology.org/ontologies/Activity.owl#Activity",
+ "http://bioontology.org/ontologies/Activity.owl#Biospecimen_Management",
+ "http://bioontology.org/ontologies/Activity.owl#Community_Engagement",
+ "http://bioontology.org/ontologies/Activity.owl#Deprecated_Activity",
+ "http://bioontology.org/ontologies/Activity.owl#Gene_Therapy",
+ "http://bioontology.org/ontologies/Activity.owl#Health_Services",
+ "http://bioontology.org/ontologies/Activity.owl#Heath_Services",
+ "http://bioontology.org/ontologies/Activity.owl#IRB",
+ "http://bioontology.org/ontologies/Activity.owl#Medical_Device_Development",
+ "http://bioontology.org/ontologies/Activity.owl#Novel_Therapeutics",
+ "http://bioontology.org/ontologies/Activity.owl#Regulatory_Compliance"]
+    ont1_terms.map! { |x| LinkedData::Models::Class.find(RDF::IRI.new(x), submission: sub1) }
+
+    ont2_terms = ["http://purl.obolibrary.org/obo/SBO_0000512",
+ "http://purl.obolibrary.org/obo/SBO_0000513",
+ "http://purl.obolibrary.org/obo/SBO_0000514",
+ "http://purl.obolibrary.org/obo/SBO_0000515",
+ "http://purl.obolibrary.org/obo/SBO_0000516",
+ "http://purl.obolibrary.org/obo/SBO_0000517",
+ "http://purl.obolibrary.org/obo/SBO_0000518",
+ "http://purl.obolibrary.org/obo/SBO_0000519",
+ "http://purl.obolibrary.org/obo/SBO_0000520",
+ "http://purl.obolibrary.org/obo/SBO_0000521",
+ "http://purl.obolibrary.org/obo/SBO_0000522"]
+    ont2_terms.map! { |x| LinkedData::Models::Class.find(RDF::IRI.new(x), submission: sub2) }
+      
+      
+    ont3_terms = ["http://purl.obolibrary.org/obo/IAO_0000178",
+ "http://purl.obolibrary.org/obo/IAO_0000179",
+ "http://purl.obolibrary.org/obo/IAO_0000180",
+ "http://purl.obolibrary.org/obo/IAO_0000181",
+ "http://purl.obolibrary.org/obo/IAO_0000182",
+ "http://purl.obolibrary.org/obo/IAO_0000183",
+ "http://purl.obolibrary.org/obo/IAO_0000184",
+ "http://purl.obolibrary.org/obo/IAO_0000185",
+ "http://purl.obolibrary.org/obo/IAO_0000186",
+ "http://purl.obolibrary.org/obo/IAO_0000225",
+ "http://purl.obolibrary.org/obo/IAO_0000300"]
+    ont3_terms.map! { |x| LinkedData::Models::Class.find(RDF::IRI.new(x), submission: sub3) }
+
+    ont1_terms.each_index do |i|
+      t1 = ont1_terms[i]
+      t2 = ont2_terms[i]
+      map = LinkedData::Models::Mapping.new()
+      map.terms = [t1,t2]
+      map.occurrence = occ12
+      assert map.valid?
+      map.save
+    end
+
+    assert LinkedData::Models::Mapping.all.length == ont1_terms.length
+
+    match = 0
+    LinkedData::Models::Mapping.all.each do |map|
+      term_ids = map.terms.map { |t| t.resource_id.value }
+      ont1_terms.each_index do |i1|
+        t1 = ont1_terms[i1]
+        if term_ids.include? t1.resource_id.value 
+          match += 1
+          assert term_ids.include?(ont2_terms[i1].resource_id.value)
+        end
+      end
+    end
+    assert match == ont1_terms.length
+    assert LinkedData::Models::Occurrence.all.length == 3
+
+    ont2_terms.each_index do |i|
+      t2 = ont2_terms[i]
+      t3 = ont3_terms[i]
+      map = LinkedData::Models::Mapping.new()
+      map.terms = [t2,t3]
+      map.occurrence = occ23
+      assert map.valid?
+      map.save
+    end
+
+    assert LinkedData::Models::Mapping.all.length == (ont1_terms.length + ont2_terms.length)
+
+    #search for two particular ontologies ont2,ont3
+    # how do I search for multiple values of the same attribute 
+    #occs = LinkedData::Models::Occurrence.where ontologies: [ ont1, ont2 ]
+
   end
 end
