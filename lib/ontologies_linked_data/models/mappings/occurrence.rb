b06ac7e3f67842ccc5670aa3030ea75da19d68b0
msalvadores <msalvadores@gmail.com>
2013-04-08 17:59:08 -0700

    instance corrections in occurrence

--- lib/ontologies_linked_data/models/mappings/occurrence.rb
+++ lib/ontologies_linked_data/models/mappings/occurrence.rb
@@ -4,13 +4,13 @@ require_relative './process'
 module LinkedData
   module Models
     class Occurrence < LinkedData::Models::Base
-      model :mapping, :name_with => lambda { |s| occurrence_id_generator(s) }
-      attribute :ontology, :cardinality => { :min => 2 }, :instance_of => { :with => LinkedData::Models::Ontology }
-      attribute :process, :not_nil => true, :instance_of => { :with => LinkedData::Models::MappingProcess }
+      model :occurrence, :name_with => lambda { |s| occurrence_id_generator(s) }
+      attribute :ontologies, :cardinality => { :min => 2 }, :instance_of => { :with => :ontology }
+      attribute :process, :not_nil => true, :single_value => true, :instance_of => { :with => :mapping_process }
 
       #rare cases
-      attribute :from, :single_value => true, :instance_of => { :with => LinkedData::Models::Class }
-      attribute :to, :single_value => true, :instance_of => { :with => LinkedData::Models::Class }
+      attribute :from, :single_value => true, :instance_of => { :with => :class }
+      attribute :to, :single_value => true, :instance_of => { :with => :class }
       attribute :relationship, :single_value => true, :instance_of => { :with => IRI }
 
       def occurrence_id_generator(inst)
