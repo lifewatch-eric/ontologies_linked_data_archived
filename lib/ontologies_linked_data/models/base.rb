fce8f9ebe3e476dbc979efc80ea8bc9cd103f004
Paul R Alexander <palexander@stanford.edu>
2013-04-23 16:07:26 -0700

    Allow for generating resource id prefix externally

--- lib/ontologies_linked_data/models/base.rb
+++ lib/ontologies_linked_data/models/base.rb
@@ -6,18 +6,20 @@ module LinkedData
     class Base < Goo::Base::Resource
       include LinkedData::Hypermedia::Resource
 
-      def self.plural_resource_id(object, model_name = nil, naming_attr = nil)
+      def self.plural_resource_id(object)
+        RDF::IRI.new("#{resource_id_prefix}#{unique_value(object, self.goop_settings[:attributes], naming_attr)}")
+      end
+
+      def self.resource_id_prefix
         model_name ||= (model_name || self.goo_name.to_s).pluralize
-        url_prefix = LinkedData.settings.rest_url_prefix || self.namespace(:default)
-        RDF::IRI.new("#{url_prefix}#{model_name}/#{unique_value(object, self.goop_settings[:attributes], naming_attr)}")
+        prefix_base = LinkedData.settings.rest_url_prefix || self.namespace(:default)
+        "#{prefix_base}#{model_name}/"
       end
 
       private
 
-      def self.unique_value(object, attributes, naming_attr = nil)
-        unless naming_attr
-          self.goop_settings[:attributes].each {|k,v| naming_attr = k if v[:validators] && v[:validators][:unique]}
-        end
+      def self.unique_value(object, attributes)
+        self.goop_settings[:attributes].each {|k,v| naming_attr = k if v[:validators] && v[:validators][:unique]}
         value = object.instance_variable_get("@"+naming_attr.to_s) || object.send(naming_attr)
         CGI.escape(value.to_s)
       end
