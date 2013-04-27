e014a46d0c72c9f1f6097e2b3f51d3c12fee0088
Paul R Alexander <palexander@stanford.edu>
2013-04-26 19:25:42 -0700

    Warn about tests against potential production machines

--- test/test_case.rb
+++ test/test_case.rb
@@ -18,9 +18,15 @@ require "webmock/minitest"
 WebMock.allow_net_connect!
 
 # Check to make sure you want to run if not pointed at localhost
-if !LinkedData.settings.goo_host.eql?("localhost")
-  print "\n\n================================== WARNING =================================="
-  print "\nYou are using a triplestore at #{LinkedData.settings.goo_host}. Tests can be destructive.\nType 'y' to continue: "
+safe_host = Regexp.new(/localhost|ncbo-dev*/)
+unless LinkedData.settings.goo_host.match(safe_host) && LinkedData.settings.search_server_url.match(safe_host) && LinkedData.settings.redis_host.match(safe_host)
+  print "\n\n================================== WARNING ==================================\n"
+  print "** TESTS CAN BE DESTRUCTIVE -- YOU ARE POINTING TO A POTENTIAL PRODUCTION/STAGE SERVER **\n"
+  print "Servers:\n"
+  print "triplestore -- #{LinkedData.settings.goo_host}\n"
+  print "search -- #{LinkedData.settings.search_server_url}\n"
+  print "redis -- #{LinkedData.settings.redis_host}\n"
+  print "Type 'y' to continue: "
   $stdout.flush
   confirm = $stdin.gets
   if !(confirm.strip == 'y')
