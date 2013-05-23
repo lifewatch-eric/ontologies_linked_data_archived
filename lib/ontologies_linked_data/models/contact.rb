b1b461f451136d65b0a435c235f5ac36463864aa
msalvadores <msalvadores@gmail.com>
2013-05-23 10:52:07 -0700

    roles

--- lib/ontologies_linked_data/models/contact.rb
+++ lib/ontologies_linked_data/models/contact.rb
@@ -1,8 +1,13 @@
 module LinkedData
   module Models
     class Contact < LinkedData::Models::Base
-      attribute :name, :single_value => true, :not_nil => true
-      attribute :email, :single_value => true, :not_nil => true
+      model :contact, name_with: lambda { |s| id_generator(s) }  
+      attribute :name, enforce: [:existence]
+      attribute :email, enforce: [:existence]
+      def self.id_generator(inst)
+        #generate uuid 
+        binding.pry
+      end
     end
   end
 end
--- lib/ontologies_linked_data/models/users/role.rb
+++ lib/ontologies_linked_data/models/users/role.rb
@@ -5,42 +5,15 @@ module LinkedData
         DEFAULT = "LIBRARIAN"
         VALUES = ["LIBRARIAN", "ADMINISTRATOR", "DEVELOPER"]
 
-        # Value stored on class so we only initialize once
-        class << self
-          attr_accessor :initialized
-        end
-
         model :role, name_with: :role
         attribute :role, enforce: [:unique, :existence]
 
-        def self.init
-          return false if self.initialized
-          VALUES.each do |role|
-            user_role = LinkedData::Models::Users::Role.new(:role => role)
-            user_role.save(force = true) unless user_role.exist?
-          end
-          self.initialized = true
-        end
+        enum VALUES
 
         def self.default
-          init
-          self.find(DEFAULT)
+          return find(DEFAULT).include(:role).first
         end
 
-        def self.find(param, store_name=nil)
-          init
-          super(param, store_name)
-        end
-
-        def save(force = false)
-          raise "Do not save this object unless from init" unless force
-          super()
-        end
-
-        def delete(force = false)
-          raise "Do not delete this object unless overriding" unless force
-          super()
-        end
       end
     end
   end
