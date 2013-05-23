4767b395565799738753b638f3887c360a9abae3
Paul R Alexander <palexander@stanford.edu>
2013-05-23 12:50:24 -0700

    Group model/test updates for Goo 1.0

--- lib/ontologies_linked_data/models/group.rb
+++ lib/ontologies_linked_data/models/group.rb
@@ -1,12 +1,12 @@
 module LinkedData
   module Models
     class Group < LinkedData::Models::Base
-      model :group, :name_with => lambda { |s| plural_resource_id(s) }
-      attribute :acronym, :unique => true, :single_value => true, :not_nil => true
-      attribute :name, :single_value => true, :not_nil => true
-      attribute :description, :single_value => true
-      attribute :created, :date_time_xsd => true, :single_value => true, :not_nil => true, :default => lambda { |record| DateTime.now }
-      attribute :ontologies, :inverse_of => { :with => :ontology, :attribute => :group }
+      model :group, name_with: :acronym
+      attribute :acronym, enforce: [:unique, :existence]
+      attribute :name, enforce: [:existence]
+      attribute :description
+      attribute :created, enforce: [:date_time], :default => lambda { |record| DateTime.now }
+      attribute :ontologies, inverse: { on: :ontology, attribute: :group }
     end
   end
 end
