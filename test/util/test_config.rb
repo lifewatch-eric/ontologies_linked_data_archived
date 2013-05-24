783c52e08bfcc0138399d82635d7454a675cc968
Paul R Alexander <palexander@stanford.edu>
2013-05-24 14:12:23 -0700

    Fix config test for Goo 1.0

--- test/util/test_config.rb
+++ test/util/test_config.rb
@@ -3,23 +3,24 @@ require_relative "../../lib/ontologies_linked_data"
 
 class TestLinkedDataSerializer < MiniTest::Unit::TestCase
   def teardown
-    Goo.class_variable_set("@@_default_store", nil)
+    Goo.class_variable_set("@@sparql_backends", {})
     LinkedData.instance_variable_set("@settings_run", false)
-    SparqlRd::Repository.class_variable_set("@@instances", {})
     load(File.expand_path("../../../config/config.rb", __FILE__))
   end
 
   def test_default_config
-    Goo.class_variable_set("@@_default_store", nil)
+    Goo.class_variable_set("@@sparql_backends", {})
     LinkedData.instance_variable_set("@settings_run", false)
     LinkedData.config()
     assert LinkedData.settings.goo_host == "localhost"
     assert LinkedData.settings.goo_port == 9000
-    assert (not Goo.store.nil?)
+    assert (not Goo.sparql_data_client.nil?)
+    assert (not Goo.sparql_update_client.nil?)
+    assert (not Goo.sparql_query_client.nil?)
   end
 
   def test_custom_config
-    test_port = 1
+    test_port = 1111
     test_host = "test_host"
 
     # Override safety check
@@ -36,6 +37,8 @@ class TestLinkedDataSerializer < MiniTest::Unit::TestCase
 
     assert LinkedData.settings.goo_host == test_host
     assert LinkedData.settings.goo_port == test_port
-    assert (not Goo.store.nil?)
+    assert (not Goo.sparql_data_client.nil?)
+    assert (not Goo.sparql_update_client.nil?)
+    assert (not Goo.sparql_query_client.nil?)
   end
 end
