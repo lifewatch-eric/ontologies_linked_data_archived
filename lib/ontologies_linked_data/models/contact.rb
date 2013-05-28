22491dcde74108e60ed96cac4779ce54942a581d
Paul R Alexander <palexander@stanford.edu>
2013-05-28 11:30:49 -0700

    Cleanup

--- lib/ontologies_linked_data/models/contact.rb
+++ lib/ontologies_linked_data/models/contact.rb
@@ -1,10 +1,9 @@
 module LinkedData
   module Models
     class Contact < LinkedData::Models::Base
-      model :contact, name_with: lambda { |c| uuid_uri_generator(c) }  
+      model :contact, name_with: lambda { |c| uuid_uri_generator(c) }
       attribute :name, enforce: [:existence]
       attribute :email, enforce: [:existence]
-
     end
   end
 end
--- lib/ontologies_linked_data/models/review.rb
+++ lib/ontologies_linked_data/models/review.rb
@@ -1,10 +1,12 @@
+require 'date'
+
 module LinkedData
   module Models
     class Review < LinkedData::Models::Base
       model :review, name_with: lambda {|s| generate_review_iri(s)}
       attribute :creator, enforce: [:user, :existence]
-      attribute :created, enforce: [:date_time], :default => lambda { |record| DateTime.now }
-      attribute :updated, enforce: [:date_time], :default => lambda {|x| DateTime.new }
+      attribute :created, enforce: [:date_time], :default => lambda {|x| DateTime.now}
+      attribute :updated, enforce: [:date_time], :default => lambda {|x| DateTime.now}
       attribute :body, enforce: [:existence]
       attribute :ontologyReviewed, enforce: [:ontology, :existence]
       attribute :usabilityRating
