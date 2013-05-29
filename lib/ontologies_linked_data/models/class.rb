9965adb2e74e641447e5c23cfaa307daffcd0dcd
msalvadores <msalvadores@gmail.com>
2013-05-28 19:55:51 -0700

    legacy self.inject_subproperty_query_option gone

--- lib/ontologies_linked_data/models/class.rb
+++ lib/ontologies_linked_data/models/class.rb
@@ -205,23 +205,6 @@ module LinkedData
         end
       end
 
-      def self.inject_subproperty_query_option(params)
-        #subPropertyOf reasoning by default if loading labels/syns/defs
-        if params.include? :load_attrs
-          unless params[:load_attrs] == :all
-            attrs = params[:load_attrs].instance_of?(Array) ? params[:load_attrs]
-                                              : params[:load_attrs].keys
-          else
-            params[:query_options] = { rules: :SUBP }
-            return
-          end
-          if attrs == :defined ||
-            !(attrs & [:prefLabel, :synonym, :definition]).empty?
-             params[:query_options] = { rules: :SUBP } if !params.include? :query_options
-          end
-
-        end
-      end
     end
 
   end
