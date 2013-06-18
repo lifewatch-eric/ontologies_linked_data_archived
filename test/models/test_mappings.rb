426ecb82446a6940335bda8bd3634b58cef6d809
msalvadores <msalvadores@gmail.com>
2013-06-17 17:24:32 -0700

    test mappings modified for goo 1.0

--- test/models/test_mappings.rb
+++ test/models/test_mappings.rb
@@ -7,6 +7,12 @@ class TestMapping < LinkedData::TestOntologyCommon
       puts "KB with too many mappings to run test. Is this pointing to a TEST KB?"
       raise Exception, "KB with too many mappings to run test. Is this pointing to a TEST KB?"
     end
+    LinkedData::Models::MappingProcess.all do |m|
+      m.delete
+    end
+    LinkedData::Models::TermMapping.all do |m|
+      m.delete
+    end
     LinkedData::Models::Mapping.all do |m|
       m.delete
     end
@@ -14,63 +20,15 @@ class TestMapping < LinkedData::TestOntologyCommon
   end
 
   def ontologies_parse()
+    return
     submission_parse("MappingOntTest1", "MappingOntTest1", "./test/data/ontology_files/BRO_v3.2.owl", 11)
     submission_parse("MappingOntTest2", "MappingOntTest2", "./test/data/ontology_files/CNO_05.owl", 22)
     submission_parse("MappingOntTest3", "MappingOntTest3", "./test/data/ontology_files/aero.owl", 33)
   end
 
-  #TODO this needs to be replaced by Paul's data genrator
-  def submission_parse( acronym, name, ontologyFile, id)
-    return if ENV["SKIP_PARSING"]
-
-    bro = LinkedData::Models::Ontology.find(acronym)
-    if not bro.nil?
-      sub = bro.submissions || []
-      if sub.length > 0
-        return if sub[0].submissionStatus.parsed?
-      end
-      sub.each do |s|
-        s.load unless s.loaded?
-        s.delete
-      end
-      bro.delete
-    end
-    ont_submision =  LinkedData::Models::OntologySubmission.new({ :submissionId => id})
-    assert (not ont_submision.valid?)
-    assert_equal 6, ont_submision.errors.length
-    uploadFilePath = LinkedData::Models::OntologySubmission.copy_file_repository(acronym, id, ontologyFile)
-    ont_submision.uploadFilePath = uploadFilePath
-    owl, bro, user, status, contact = submission_dependent_objects("OWL", acronym, "test_linked_models", "UPLOADED",name)
-    bro.administeredBy = user
-    ont_submision.contact = contact
-    ont_submision.released = DateTime.now - 4
-    ont_submision.hasOntologyLanguage = owl
-    ont_submision.ontology = bro
-    ont_submision.submissionStatus = status
-    assert (ont_submision.valid?)
-    ont_submision.save
-
-    assert_equal true, ont_submision.exist?(reload=true)
-    uploaded = LinkedData::Models::SubmissionStatus.find("UPLOADED")
-    uploded_ontologies = uploaded.submissions
-    uploaded_ont = nil
-    uploded_ontologies.each do |ont|
-      ont.load unless ont.loaded?
-      ont.ontology.load unless ont.ontology.loaded?
-      if ont.ontology.acronym == acronym
-        uploaded_ont = ont
-      end
-    end
-    assert (not uploaded_ont.nil?)
-    if not uploaded_ont.ontology.loaded?
-      uploaded_ont.ontology.load
-    end
-    uploaded_ont.process_submission Logger.new(STDOUT)
-  end
-
   def get_process(name)
     #just some user
-    user = LinkedData::Models::User.all[0]
+    user = LinkedData::Models::User.where.include(:username).all[0]
 
     #process
     ps = LinkedData::Models::MappingProcess.where({:name => name })
@@ -80,7 +38,7 @@ class TestMapping < LinkedData::TestOntologyCommon
     p = LinkedData::Models::MappingProcess.new(:owner => user, :name => name)
     assert p.valid?
     p.save
-    ps = LinkedData::Models::MappingProcess.where({:name => name })
+    ps = LinkedData::Models::MappingProcess.where({:name => name }).to_a
     assert ps.length == 1
     return ps[0]
   end
@@ -89,11 +47,11 @@ class TestMapping < LinkedData::TestOntologyCommon
 
     process = get_process("LOOMTEST") 
 
-    ont1 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest1" })[0]
+    ont1 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest1" }).to_a[0]
     sub1 = ont1.latest_submission
-    ont2 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest2" })[0]
+    ont2 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest2" }).to_a[0]
     sub2 = ont2.latest_submission
-    ont3 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest3" })[0]
+    ont3 = LinkedData::Models::Ontology.where({ :acronym => "MappingOntTest3" }).to_a[0]
     sub3 = ont3.latest_submission
     LinkedData::Models::Mapping.all.each do |occ|
       occ.delete
@@ -140,9 +98,11 @@ class TestMapping < LinkedData::TestOntologyCommon
  "http://purl.obolibrary.org/obo/IAO_0000300"]
 
     ont1_terms_uris.each_index do |i|
-      tm1 = LinkedData::Models::TermMapping.new(term: RDF::IRI.new(ont1_terms_uris[i]), ontology: ont1.resource_id)
-      tm2 = LinkedData::Models::TermMapping.new(term: RDF::IRI.new(ont2_terms_uris[i]), ontology: ont2.resource_id)
-      map = LinkedData::Models::Mapping.new(terms: [tm1, tm2], process: process)
+      tm1 = LinkedData::Models::TermMapping.new(term: [RDF::IRI.new(ont1_terms_uris[i])], ontology: ont1)
+      tm1.save
+      tm2 = LinkedData::Models::TermMapping.new(term: [RDF::IRI.new(ont2_terms_uris[i])], ontology: ont2)
+      tm2.save
+      map = LinkedData::Models::Mapping.new(terms: [tm1, tm2], process: [process])
       map.terms = [tm1,tm2]
       assert map.valid?
       map.save
@@ -150,7 +110,8 @@ class TestMapping < LinkedData::TestOntologyCommon
 
     assert LinkedData::Models::Mapping.all.length == ont1_terms_uris.length
 
-    LinkedData::Models::Mapping.all.each do |map|
+    mappings = LinkedData::Models::Mapping.where.include(terms:[ ontology: :acronym]).to_a
+    mappings.each do |map|                                           
       ont1_index = 0
       ont2_index = 1
       if map.terms[0].ontology.acronym != "MappingOntTest1"
@@ -164,11 +125,12 @@ class TestMapping < LinkedData::TestOntologyCommon
 
     ont2_terms_uris.each_index do |i|
       #reusing TermMapping
-      tm2 = LinkedData::Models::TermMapping.new(term: ont2_terms_uris[i], ontology: ont2.resource_id)
+      tm2 = LinkedData::Models::TermMapping.new(term: [ont2_terms_uris[i]], ontology: ont2)
       assert  tm2.exist?
-      tm2 = LinkedData::Models::TermMapping.find(LinkedData::Models::TermMapping.term_mapping_id_generator(tm2))
-      tm3 = LinkedData::Models::TermMapping.new(term: RDF::IRI.new(ont3_terms_uris[i]), ontology: ont3.resource_id)
-      map = LinkedData::Models::Mapping.new(terms: [tm2, tm3], process: process)
+      tm2 = LinkedData::Models::TermMapping.find(LinkedData::Models::TermMapping.term_mapping_id_generator(tm2)).first
+      tm3 = LinkedData::Models::TermMapping.new(term: [RDF::IRI.new(ont3_terms_uris[i])], ontology: ont3)
+      tm3.save
+      map = LinkedData::Models::Mapping.new(terms: [tm2, tm3], process: [process])
       map.terms = [tm2,tm3]
       assert map.valid?
       map.save
@@ -176,20 +138,17 @@ class TestMapping < LinkedData::TestOntologyCommon
 
     assert LinkedData::Models::Mapping.all.length == (ont1_terms_uris.length + ont2_terms_uris.length)
 
-    mappings = LinkedData::Models::Mapping.where terms: { ontology: ont1.resource_id }
+    mappings = LinkedData::Models::Mapping.where(terms: [ ontology: ont1]).to_a
     assert mappings.length == 11
 
-    mappings = LinkedData::Models::Mapping.where terms: { ontology: ont2.resource_id }
+    mappings = LinkedData::Models::Mapping.where(terms: [ ontology: ont2 ]).to_a
     assert mappings.length == 22
 
-    mappings = LinkedData::Models::Mapping.where terms: [{ ontology: ont1.resource_id }, { ontology: ont2.resource_id }]
+    mappings = LinkedData::Models::Mapping.where(terms: [ ontology: ont1 ])
+                                            .and(terms: [ ontology: ont2 ]).to_a
     assert mappings.length == 11
 
-
-    mappings = LinkedData::Models::Mapping.where terms: { ontology: [ont1.resource_id, ont2.resource_id ] }
-    assert mappings.length == 0
-
-    mappings = LinkedData::Models::Mapping.where terms: [{ ontology: ont3.resource_id }]
+    mappings = LinkedData::Models::Mapping.where(terms: [ ontology: ont3 ]).to_a
     assert mappings.length == 11
   end
 end
