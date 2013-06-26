1623dbcbdf2526a19c5216b672fc44ef30d69988
msalvadores <msalvadores@gmail.com>
2013-06-25 19:38:20 -0700

    set of calls to create mapping and mapping terms. It uses redis cache to speed up the process.

--- lib/ontologies_linked_data/mappings/mappings.rb
+++ lib/ontologies_linked_data/mappings/mappings.rb
@@ -1,2 +1,79 @@
 require_relative "batch"
 require_relative "loom"
+
+module LinkedData
+module Mappings
+
+  def self.mapping_key(id)
+    return "mappings:mapping:#{id.to_s}"
+  end
+
+  def self.mapping_procs_key(id)
+    return "mappings:mapping:procs:#{id.to_s}"
+  end
+
+  def self.term_mapping_key(id)
+    return "mappings:terms:#{id.to_s}"
+  end
+
+  def self.create_term_mapping(term_uris,acronym)
+    id_term_mapping = LinkedData::Models::TermMapping.term_mapping_id_generator(term_uris,acronym)
+    return id_term_mapping if exist_term_mapping?(term_uris,acronym)
+    term = LinkedData::Models::TermMapping.new
+    term.ontology = LinkedData::Models::Ontology.find(acronym).include(:acronym).first
+    term.term = term_uris
+    term.save
+    term_m_key = term_mapping_key(id_term_mapping)
+    redis = LinkedData::Mappings::Batch.redis_cache
+    redis.hset term_m_key, "ontology", acronym
+    redis.hset term_m_key, "terms", term_uris.map { |x| x.to_s }.join("|split|")
+    return id_term_mapping
+  end
+
+  def self.exist_term_mapping?(term_uris,ontology)
+    redis = LinkedData::Mappings::Batch.redis_cache
+    id_term_mapping = LinkedData::Models::TermMapping.term_mapping_id_generator(term_uris,ontology)
+    return redis.exists(term_mapping_key(id_term_mapping))
+  end
+
+  def self.create_mapping(term_mapping_ids)
+    id_mapping = LinkedData::Models::Mapping.mapping_id_generator_iris(*term_mapping_ids)
+    return id_mapping if exist_mapping?(term_mapping_ids)
+    mapping = LinkedData::Models::Mapping.new
+    term_mappings = term_mapping_ids.map { |x| LinkedData::Models::TermMapping.find(x).first }
+    mapping.terms = term_mappings
+    mapping.save
+    redis = LinkedData::Mappings::Batch.redis_cache
+    redis.rpush(mapping_key(id_mapping), term_mapping_ids)
+    return id_mapping
+  end
+  
+  def self.exist_mapping?(term_mapping_ids)
+    redis = LinkedData::Mappings::Batch.redis_cache
+    id_mapping = LinkedData::Models::Mapping.mapping_id_generator_iris(*term_mapping_ids)
+    return redis.exists(mapping_key(id_mapping))
+  end
+
+  def self.connect_mapping_process(mapping_id,process)
+    redis = LinkedData::Mappings::Batch.redis_cache
+    unless redis.exists(mapping_key(mapping_id))
+      raise ArgumentError, "Mapping id #{mapping_id.to_ntriples} not found"
+    end
+    map_proc_key = mapping_procs_key(mapping_id)
+    procs = redis.lrange(map_proc_key,0,-1)
+    return if procs.index(process.id.to_s)
+    mapping = LinkedData::Models::Mapping.find(mapping_id)
+                                            .include(:process)
+                                            .include(:terms)
+                                            .first
+    map_procs = mapping.process.dup
+    return if map_procs.map { |x| x.id.to_s }.index(process.id.to_s)
+    map_procs << process
+    mapping.process = map_procs
+    mapping.save
+    redis.rpush(map_proc_key, [process.id.to_s])
+    nil
+  end
+
+end
+end
