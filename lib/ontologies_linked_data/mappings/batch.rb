5c68d7523989791a327cf2b9c7a625764baf288b
msalvadores <msalvadores@gmail.com>
2013-07-01 14:22:52 -0700

    general functionality for automatic mappings move to batch

--- lib/ontologies_linked_data/mappings/batch.rb
+++ lib/ontologies_linked_data/mappings/batch.rb
@@ -51,8 +51,17 @@ module LinkedData
     #TODO end this unnecesary methods
 
     class BatchProcess
-      def initialize(process_name,logger,*onts)
+      def initialize(process_name,paging,dumper,
+                     line_parser,ok_mapping,skip_mapping,
+                     logger,sort_field,*onts)
+        @record_tuple = Struct.new(:record_a,:record_b)
         @process_name = process_name
+        @paging = paging
+        @dumper = dumper
+        @ok_mapping = ok_mapping
+        @skip_mapping = skip_mapping
+        @line_parser = line_parser
+        @sort_field = sort_field
         @logger = logger || Logger.new(STDOUT)
         process = get_process(process_name)
         @logger.info("using process id #{process.id.to_ntriples}")
@@ -64,10 +73,6 @@ module LinkedData
         @ontologies = onts
         @process = process
         raise Exception "Only support for two ontologies" if @ontologies.length != 2
-        @term_mappings = {}
-        @existing_mappings_other_process = {}
-        @existing_mappings_same_process = {}
-        @new_mappings = {}
       end
 
       def get_process(name)
@@ -112,52 +117,89 @@ module LinkedData
         return ont_folder
       end
 
-      def load_cache()
-        #this load in memory all the mappings for the two ontologies
-        #keep in the cache only the ones that have procs
-      end
-
       def start()
-        unless self.respond_to?(:run)
-          raise ArgumentError, "Batch Process '#{@process_name}' needs to implement 'run()'"
-        end
-
         t0 = Time.now 
         @logger.info("Starting batch ...")
-        run()
+        label_dumps_file_paths = dumps()
+        sorted_file_path = sort(label_dumps_file_paths,@sort_field)
+        mapping_pairs = process_sort_results(sorted_file_path)
+        mapping_pairs.each do |pair|
+          id_t_a = LinkedData::Mappings.create_term_mapping([pair.record_a.term_id],
+                                    pair.record_a.acronym)
+          id_t_b = LinkedData::Mappings.create_term_mapping([pair.record_b.term_id],
+                                    pair.record_b.acronym)
+          mapping_id = LinkedData::Mappings.create_mapping([id_t_a, id_t_b])
+          LinkedData::Mappings.connect_mapping_process(mapping_id, @process)
+        end
         @logger.info("Total batch process time #{Time.now - t0} sec.")
-
       end
 
-      def new_mapping(*term_mappings)
-        id = LinkedData::Models::Mapping.mapping_id_generator_iris(*term_mappings)
-        if @existing_mappings_same_process.include?(id)
-          return
-        end
-        if @existing_mappings_other_process.include?(id)
-          #update process
-           m=@existing_mappings_other_process[id]
-           m.process = (m.process.dup << @process)
-           m.save
-           return
-        end
-        term_mappings_in = []
-        term_mappings.each do |tm|
-          id_tm = TermMapping.term_mapping_id_generator(tm)
-          term_mappings_in << @term_mappings.include?(id_tm) ? @term_mappings[id_tm] : tm 
-        end
-
-        term_mappings_in.each do |tm|
-          unless tm.persistent?
-            tm.save
-            @term_mappings[tm.id] = tm
+      def process_sort_results(sorted_labels_file)
+        record_a = nil
+        record_b = nil
+        tuples = []
+        backlog = []
+        File.open(sorted_labels_file,"r").each do |line|
+          if record_b && (record_a.cui == record_b.cui)
+            backlog << record_b
+          else
+            backlog = []
+          end
+          record_b = record_a
+          record_a = @line_parser.call(line)
+          if record_a && record_b
+            next if @skip_mapping.call(record_a,record_b)
+            if @ok_mapping.call(record_a,record_b)
+              tuples << create_record_tuple(record_a,record_b)
+              if backlog.length > 0
+                backlog.each do |back_log_tuple|
+                  tuples << create_record_tuple(record_a,back_log_tuple)
+                end
+              end
+            end
           end
         end
-        mapping = create_or_retrieve_mapping(term_mappings_in)
-        @existing_mappings_same_process[mapping.id] = mapping
+        return tuples
       end
 
-      def self.create_ontology_dump(ont,proc_name,paging,dumper,logger=nil)
+      def dumps()
+        dump_paths = []
+        @ontologies.each do |ont|
+          ont.bring(submissions: [:submissionId])
+          latest_submission = ont.latest_submission
+          dump_paths << create_ontology_dump(ont,@process_name,
+                                            @paging,@dumper,logger=@logger)
+        end
+        return dump_paths
+      end
+
+      def sort(dump_files,field)
+        ontologies_sorted = @ontologies.sort_by { |ont| ont.acronym }
+        ont_first = ontologies_sorted.first
+        sorted_file = File.join([BatchProcess.mappings_ontology_folder(ont_first),
+                       "aggregated_cui_labels_with_#{ontologies_sorted.last.acronym}.txt"])
+        sort_command = "cat " + (dump_files.join " ") + "| sort -t, -k#{field}"
+        if $TMP_SORT_FOLDER
+          if not Dir.exist?($TMP_SORT_FOLDER)
+            FileUtils.mkdir_p($TMP_SORT_FOLDER)
+          end
+          sort_command += " -T #{$TMP_SORT_FOLDER}"
+        end
+        sort_command += " > #{sorted_file}"
+        @logger.info("sort_command: #{sort_command}")
+        t0 = Time.now
+        stdin, stdout, stderr, wait_thr = Open3.popen3(sort_command)
+        unless wait_thr.value.success?
+          @logger.error("error in sort command pid:#{wait_thr.pid}, exit code #{wait_thr.value}")
+          @logger.error("error in sort command pid:#{wait_thr.pid}, err trace #{stderr.read}")
+          @logger.error("error in sort command pid:#{wait_thr.pid}, out trace #{stdout.read}")
+          raise Exception, "Error in sort command '#{sort_command}'"
+        end
+        @logger.info("sort success. run in #{Time.now - t0} sec.")
+        return sorted_file
+      end
+
+      def create_ontology_dump(ont,proc_name,paging,dumper,logger=nil)
         ont.bring(submissions: [:submissionId])
         latest_submission = ont.latest_submission
 
@@ -172,11 +214,13 @@ module LinkedData
           page_i = 1
           page = nil
           entry_count = 0
+          paging.in(latest_submission)
+          paging.page(page_i,2500)
           CSV::open(dump_file_path,'wb') do |csv|
             begin
               page = paging.all
               page.each do |c|
-                dumper.call(c).each do |entry|
+                dumper.call(c,ont).each do |entry|
                   csv << entry
                   entry_count += 1
                 end
