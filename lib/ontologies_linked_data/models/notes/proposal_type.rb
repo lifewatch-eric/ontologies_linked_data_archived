1e9b81b02deec267e98df416b891b076f6ad4a0d
Paul R Alexander <palexander@stanford.edu>
2013-06-30 22:04:20 -0700

    Rename details -> proposal_type

--- lib/ontologies_linked_data/models/notes/proposal_type.rb
+++ lib/ontologies_linked_data/models/notes/proposal_type.rb
@@ -1,13 +1,15 @@
 module LinkedData
   module Models
     module Notes
-      class Details < LinkedData::Models::Base
+      class ProposalType < LinkedData::Models::Base
         DEFAULT = "ProposalNewClass"
         VALUES = ["ProposalNewClass", "ProposalChangeHierarchy", "ProposalChangeProperty"]
 
         model :details, name_with: :type
         attribute :type, enforce: [:existence, :unique]
         enum VALUES
+
+        embedded true
       end
     end
   end
