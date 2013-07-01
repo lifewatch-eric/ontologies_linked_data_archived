35faed1e1472ef71e60b0316ab60b5f0334ba686
msalvadores <msalvadores@gmail.com>
2013-07-01 15:50:13 -0700

    removal of legacy code in mappings - it is not used.

--- lib/ontologies_linked_data/mappings/batch.rb
+++ lib/ontologies_linked_data/mappings/batch.rb
@@ -14,44 +14,6 @@ module LinkedData
       end
      end
 
-    #TODO these methods are not really necessary
-    def self.exist?(*term_mappings)
-     id = LinkedData::Models::Mapping.mapping_id_generator_iris(*term_mappings)
-      
-     query =  
-       """SELECT * WHERE { GRAPH ?g { <#{id.value}> a <#{LinkedData::Models::Mapping.type_uri}> }} LIMIT 1"""
-     epr = Goo.store(@store_name)
-     epr.query(query).each_solution do |sol|
-       return true
-     end
-     return false
-    end
-
-    #it only creates the id
-    def self.create_or_retrieve_mapping(*term_mappings)
-     id = LinkedData::Models::Mapping.mapping_id_generator_iris(*term_mappings)
-     if Mappings.exist?(*term_mappings)
-       return LinkedData::Models::Mapping.find(id)
-     end
-     assign = []
-     term_mappings.each do |tm|
-       assign << LinkedData::Models::TermMapping.find(tm.resource_id) || tm
-     end
-     m = LinkedData::Models::Mapping.new(terms: term_mappings)
-     m.save
-     return  m
-    end
-
-    def self.update_mapping_process(map,pro)
-      map.process.each do |p|
-        return p.id == pro.id
-      end
-      procs = map.process.dup << pro
-      map.process = procs
-      map.save
-    end
-    #TODO end this unnecesary methods
-
     class BatchProcess
       def initialize(process_name,paging,dumper,
                      line_parser,ok_mapping,skip_mapping,
