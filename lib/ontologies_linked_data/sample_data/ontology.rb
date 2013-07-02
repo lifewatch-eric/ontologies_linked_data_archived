d8649a0a56db2d71d4fa4cc59cc1abf5f8503449
msalvadores <msalvadores@gmail.com>
2013-07-02 15:47:50 -0700

    sample ontology can parse now ontology files that only reside in API tests

--- lib/ontologies_linked_data/sample_data/ontology.rb
+++ lib/ontologies_linked_data/sample_data/ontology.rb
@@ -62,7 +62,11 @@ module LinkedData
             if process_submission && (submissions_to_process.nil? || submissions_to_process.include?(os.submissionId))
               file_path = options[:file_path] 
               file_path = "../../../../test/data/ontology_files/BRO_v3.#{os.submissionId}.owl" if file_path.nil?
-              file_path = File.expand_path(file_path, __FILE__)
+              if File.exist?(file_path)
+                file_path = File.expand_path(file_path)
+              else
+                file_path = File.expand_path(file_path, __FILE__)
+              end
               raise ArgumentError, "File located at #{file_path} does not exist" unless File.exist?(file_path)
               if os.submissionId > 2
                 raise ArgumentError, "create_ontologies_and_submissions does not support process submission with more than 2 versions"
