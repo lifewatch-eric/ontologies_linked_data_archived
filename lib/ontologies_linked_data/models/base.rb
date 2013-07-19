01193b7916e13079ee0023e058b8c797297140cd
Paul R Alexander <palexander@stanford.edu>
2013-07-18 19:03:59 -0700

    Make all LinkedData::Models CachableResources

--- lib/ontologies_linked_data/models/base.rb
+++ lib/ontologies_linked_data/models/base.rb
@@ -5,6 +5,7 @@ module LinkedData
   module Models
     class Base < Goo::Base::Resource
       include LinkedData::Hypermedia::Resource
+      include LinkedData::HTTPCache::CachableResource
     end
   end
 end
