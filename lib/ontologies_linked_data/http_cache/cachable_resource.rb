ed4f6ad6c5975e206767188e938bb8739f14d7e8
Paul R Alexander <palexander@stanford.edu>
2013-07-18 19:03:38 -0700

    Add CachableResource to enable HTTP cache methods

---
+++ lib/ontologies_linked_data/http_cache/cachable_resource.rb
@@ -0,0 +1,169 @@
+require 'digest/md5'
+require 'time'
+
+module LinkedData::HTTPCache
+  REDIS = Redis.new(host: LinkedData.settings.redis_host, port: LinkedData.settings.redis_port)
+  def self.invalidate_all
+    key_set = LinkedData::HTTPCache::CachableResource::KEY_SET
+    keys = REDIS.smembers(key_set) rescue binding.pry
+    if keys
+      keys.each_slice(500_000) {|chunk| REDIS.del chunk}
+      REDIS.del key_set
+    end
+  end
+
+  module CachableResource
+    KEY_SET = "httpcache:keys"
+    KEY_PREFIX = "httpcache:last_modified"
+    COLLECTION_KEY_PREFIX = "httpcache:last_modified:collection"
+
+    ##
+    # Compare a last modified string to the last modified time for this object
+    def last_modified_valid?(last_modified)
+      cache_read.eql?(last_modified)
+    end
+
+    ##
+    # Invalidate the cache entry for this object
+    def cache_invalidate
+      REDIS.hdel cache_prefix_and_segment, cache_key
+      self.class.cache_collection_invalidate
+    end
+
+    ##
+    # Invalidate all entries in the segment to which this object belongs
+    def cache_segment_invalidate
+      REDIS.del cache_prefix_and_segment
+    end
+
+    ##
+    # Update the last modified date in the cache for this object
+    def cache_write
+      REDIS.hmset cache_prefix_and_segment, cache_key, Time.now.httpdate
+      REDIS.sadd self.class.key_set, cache_prefix_and_segment
+      self.class.cache_collection_write
+    end
+
+    ##
+    # The last modified time for this object
+    def cache_read
+      (REDIS.hmget(cache_prefix_and_segment, cache_key) || []).first
+    end
+    alias :last_modified :cache_read
+
+    ##
+    # The cache key for this object
+    def cache_key
+      if self.class.cache_settings[:cache_key]
+        self.class.cache_settings[:cache_key].call(self)
+      else
+        Digest::MD5.hexdigest(self.id)
+      end
+    end
+
+    ##
+    # The cache key and the segment for the current object
+    def cache_prefix_and_segment
+      self.class.key_prefix + current_segment rescue binding.pry
+    end
+
+    ##
+    # The object's current segment
+    def current_segment
+      segment = self.class.cache_settings[:cache_segment]
+      return "" if segment.nil?
+      ":#{segment.call(self).join(':')}"
+    end
+
+    module ClassMethods
+      ##
+      # Wrappers for constants so they are available in instance
+      def key_set() KEY_SET end
+      def key_prefix() KEY_PREFIX end
+
+      ##
+      # Update the collection last modified date
+      def cache_collection_write
+        REDIS.hmset cache_collection_prefix, cache_collection_key, Time.now.httpdate
+        REDIS.sadd KEY_SET, cache_collection_prefix
+      end
+
+      ##
+      # Get the collection last modified date
+      def cache_collection_read
+        (REDIS.hmget(cache_collection_prefix, cache_collection_key) || []).first
+      end
+
+      ##
+      # Get the collection last modified date
+      def collection_last_modified
+        cache_collection_read
+      end
+
+      ##
+      # Compare a last modified string to the last modified time for this collection
+      def collection_last_modified_valid?(last_modified)
+        cache_collection_read.eql?(last_modified)
+      end
+
+      ##
+      # Invalidate the cache entry for this collection
+      def cache_collection_invalidate
+        REDIS.hdel cache_collection_prefix, cache_collection_key
+      end
+
+      ##
+      # The cache key for this collection
+      def cache_collection_key
+        name
+      end
+
+      ##
+      # The prefix for this collection
+      def cache_collection_prefix
+        COLLECTION_KEY_PREFIX
+      end
+    end
+
+    # Internal
+
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    def self.store_settings(cls, type, setting)
+      cls.hypermedia_settings ||= {}
+      cls.hypermedia_settings[type] = setting
+    end
+
+    module ClassMethods
+      # KEY_SET = LinkedData::HTTPCache::CachableResource::KEY_SET
+      # KEY_PREFIX = LinkedData::HTTPCache::CachableResource::KEY_PREFIX
+      # COLLECTION_KEY_PREFIX = LinkedData::HTTPCache::CachableResource::COLLECTION_KEY_PREFIX
+
+      attr_accessor :cache_settings
+      def cache_settings
+        @cache_settings || {}
+      end
+
+      # Methods with these names will be created
+      # for each entry, allowing values to be
+      # stored on a per-class basis
+      SETTINGS = [
+        :cache_key,
+        :cache_timeout,
+        :cache_segment
+      ]
+
+      ##
+      # Write methods on the class based on settings names
+      SETTINGS.each do |method_name|
+        define_method method_name do |*args|
+          CachableResource.store_settings(self, method_name, args)
+        end
+      end
+    end
+  end
+end
+
+
---
+++ test/http_cache/test_http_cache.rb
@@ -0,0 +1,84 @@
+require_relative "../test_case"
+
+class TestHTTPCache < LinkedData::TestCase
+
+  def self.before_suite
+    self.new("before_suite").delete_ontologies_and_submissions
+    @@ontology, @@cls = self.new("before_suite")._ontology_and_class
+  end
+
+  def self.after_suite
+    self.new("after_suite").delete_ontologies_and_submissions
+    LinkedData::HTTPCache.invalidate_all
+  end
+
+  def _ontology_and_class
+    results = create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: true)
+    ontology = results[2].first
+    cls = LinkedData::Models::Class.where.include(:prefLabel).in(ontology.latest_submission).page(1, 1).first
+    return ontology, cls
+  end
+
+  def teardown
+    LinkedData::HTTPCache.invalidate_all
+  end
+
+  def test_last_modified_valid
+    @@ontology.cache_write
+    assert @@ontology.last_modified
+    assert @@ontology.last_modified_valid?(@@ontology.last_modified)
+  end
+
+  def test_cache_invalidate
+    @@ontology.cache_write
+    assert @@ontology.last_modified
+    @@ontology.cache_invalidate
+    assert_nil @@ontology.last_modified
+  end
+
+  def test_cache_segment_invalidate
+    classes = LinkedData::Models::Class.where.include(:prefLabel).in(@@ontology.latest_submission).page(1, 100).to_a
+    classes.each {|c| c.cache_write}
+    last_modified_values = classes.map {|c| c.last_modified}
+    assert last_modified_values.length == classes.length
+    classes.first.cache_segment_invalidate
+    last_modified_values = (classes.map {|c| c.last_modified}).compact
+    assert last_modified_values.empty?
+  end
+
+  def test_current_segment
+    @@cls.current_segment.eql?("BRO")
+  end
+
+  def test_collection_last_modified_valid
+    assert_nil LinkedData::Models::Ontology.collection_last_modified
+    LinkedData::Models::Ontology.cache_collection_write
+    assert LinkedData::Models::Ontology.collection_last_modified
+    assert LinkedData::Models::Ontology.collection_last_modified_valid?(LinkedData::Models::Ontology.collection_last_modified)
+  end
+
+  def test_cache_collection_invalidate
+    assert_nil LinkedData::Models::Ontology.collection_last_modified
+    LinkedData::Models::Ontology.cache_collection_write
+    assert LinkedData::Models::Ontology.collection_last_modified
+    LinkedData::Models::Ontology.cache_collection_invalidate
+    assert_nil LinkedData::Models::Ontology.collection_last_modified
+  end
+
+  def test_cache_invalidate_all
+    LinkedData::Models::Ontology.cache_collection_write
+    LinkedData::Models::Class.cache_collection_write
+    assert LinkedData::Models::Ontology.collection_last_modified
+    assert LinkedData::Models::Class.collection_last_modified
+    classes = LinkedData::Models::Class.where.include(:prefLabel).in(@@ontology.latest_submission).page(1, 100).to_a
+    classes.each {|c| c.cache_write}
+    last_modified_values = classes.map {|c| c.last_modified}
+    assert last_modified_values.length == classes.length
+    LinkedData::HTTPCache.invalidate_all
+    assert_nil LinkedData::Models::Ontology.collection_last_modified
+    assert_nil LinkedData::Models::Class.collection_last_modified
+    last_modified_values = (classes.map {|c| c.last_modified}).compact
+    assert last_modified_values.empty?
+  end
+
+end
