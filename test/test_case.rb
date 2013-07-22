33dc4360bf890411228cde9d697db4166b0092b2
msalvadores <msalvadores@gmail.com>
2013-07-22 14:08:13 -0700

    test case submission_dependent_objects fixed. It did not fail because was not used. Used by metrics tests now.

--- test/test_case.rb
+++ test/test_case.rb
@@ -68,41 +68,28 @@ module LinkedData
   class TestCase < MiniTest::Unit::TestCase
     def submission_dependent_objects(format,acronym,user_name,status_code)
       #ontology format
-      LinkedData::Models::OntologyFormat.init
-      owl = LinkedData::Models::OntologyFormat.where(:acronym => format)[0]
+      owl = LinkedData::Models::OntologyFormat.where(:acronym => format).first
       assert_instance_of LinkedData::Models::OntologyFormat, owl
 
       #ontology
-      LinkedData::Models::OntologyFormat.init
-      ont = LinkedData::Models::Ontology.where(:acronym => acronym)
-      LinkedData::Models::OntologyFormat.init
-      assert(ont.length < 2)
-      if ont.length == 0
-        ont = LinkedData::Models::Ontology.new({:acronym => acronym})
-      else
-        ont = ont[0]
-      end
-
-      #user test_linked_models
-      users = LinkedData::Models::User.where(:username => user_name)
-      assert(users.length < 2)
-      if users.length == 0
+      users = LinkedData::Models::User.where(:username => user_name).all
+      user = users.first
+      if user.nil?
         user = LinkedData::Models::User.new({:username => user_name})
-      else
-        user = users[0]
+        user.email = "a@example.org"
+        user.passwordHash = "XXXXX"
+        user.save
       end
+      status = LinkedData::Models::SubmissionStatus.where(:code => status_code).first
 
-          #user test_linked_models
-      LinkedData::Models::SubmissionStatus.init
-      status = LinkedData::Models::SubmissionStatus.where(:code => status_code)
-      assert(status.length < 2)
-      if status.length == 0
-        status = LinkedData::Models::SubmissionStatus.new({:code => status_code})
-      else
-        status = status[0]
+      ont = LinkedData::Models::Ontology.where(:acronym => acronym).all
+      ont = ont.first
+      if ont.nil?
+        ont = LinkedData::Models::Ontology.new({:acronym => acronym})
+        ont.name = "some name for #{acronym}"
+        ont.administeredBy = [user]
+        ont.save
       end
-
-      #Submission Status
       return owl, ont, user, status
     end
 
