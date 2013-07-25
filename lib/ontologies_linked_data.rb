d649bebdbe16889f681e5ac9aa0d697bc07d00a3
Paul R Alexander <palexander@stanford.edu>
2013-07-25 12:30:12 -0700

    Add access control to ontologies linked data

--- lib/ontologies_linked_data.rb
+++ lib/ontologies_linked_data.rb
@@ -5,6 +5,7 @@ require_relative "ontologies_linked_data/config/config"
 
 # Include other dependent code
 require_relative "ontologies_linked_data/security/authorization"
+require_relative "ontologies_linked_data/security/access_control"
 require_relative "ontologies_linked_data/hypermedia/hypermedia"
 require_relative "ontologies_linked_data/serializer"
 require_relative "ontologies_linked_data/serializers/serializers"
