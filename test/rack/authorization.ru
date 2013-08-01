212afdb71dded50e37b8dc82d16ce6d5ffe6a0e7
Paul R Alexander <palexander@stanford.edu>
2013-08-01 15:32:50 -0700

    Better authorization middleware testing

--- test/rack/authorization.ru
+++ test/rack/authorization.ru
@@ -1,7 +1,12 @@
 # This is a test rack app for use with the linked data serializer tests
 
+require 'json'
 use LinkedData::Security::Authorization
 
-map '/' do
-  run Proc.new { |env| [200, {'Content-Type' => 'text/html'}, ['success']] }
+map '/ontologies' do
+  run Proc.new { |env|
+    user = env["REMOTE_USER"]
+    apikey = user ? user.apikey : "NO USER FOUND"
+    [200, {'Content-Type' => 'text/html'}, [apikey.to_json]]
+  }
 end
--- test/rack/test_request_authorization.rb
+++ test/rack/test_request_authorization.rb
@@ -1,8 +1,12 @@
 require "test/unit"
 require "rack/test"
 require "json"
+require "logger"
 require_relative "../../lib/ontologies_linked_data"
 require_relative "../../config/config.rb"
+
+LOGGER = Logger.new($stdout)
+ENV["rack.test"] = "true"
 
 
 class TestRackAuthorization < MiniTest::Unit::TestCase
@@ -15,9 +19,13 @@ class TestRackAuthorization < MiniTest::Unit::TestCase
   def setup
     _variables
     _delete_user
-    user = _create_user
+    users = _create_user
+    user = users.shift
+    user1 = users.shift
     @apikey = user.apikey
+    @userapikey = user1.apikey
     @security = LinkedData.settings.enable_security
+    LinkedData.settings.enable_security = true
   end
 
   def teardown
@@ -26,31 +34,57 @@ class TestRackAuthorization < MiniTest::Unit::TestCase
   end
 
   def _variables
-    @username = "test_username"
+    @usernames = ["test_username", "user2"]
   end
 
   def _create_user
-    user = LinkedData::Models::User.new({
-      username: @username,
-      password: "test_password",
-      email: "test_email@example.org"
-    })
-    user.save unless user.exist?
+    users = []
+    @usernames.each do |username|
+      user = LinkedData::Models::User.new({
+        username: username,
+        password: "test_password",
+        email: "test_email@example.org"
+      })
+      user.save unless user.exist?
+      users << user
+    end
+    users
   end
 
   def _delete_user
-    user = LinkedData::Models::User.find(@username).first
-    user.delete unless user.nil?
+    @usernames.each do |username|
+      user = LinkedData::Models::User.find(username).first
+      user.delete unless user.nil?
+    end
   end
 
   def test_authorize
-    LinkedData.settings.enable_security = true
-    get "/", {}, {"Authorization" => 'apikey token="'+@apikey+'"'}
-    assert last_response.status != 403
-    get "/", {}, {"Authorization" => "apikey token=#{@apikey}"}
-    assert last_response.status != 403
-    get "/?apikey=#{@apikey}"
-    assert last_response.status != 403
+    get "/ontologies"
+    assert last_response.status == 401
+    get "/ontologies", {}, {"Authorization" => 'apikey token="'+@apikey+''+'"'}
+    assert last_response.status == 200
+    apikey = MultiJson.load(last_response.body)
+    assert @apikey.eql?(apikey)
+    get "/ontologies", {}, {"Authorization" => "apikey token=#{@apikey}"}
+    assert last_response.status == 200
+    apikey = MultiJson.load(last_response.body)
+    assert @apikey.eql?(apikey)
+    get "/ontologies?apikey=#{@apikey}"
+    assert last_response.status == 200
+    apikey = MultiJson.load(last_response.body)
+    assert @apikey.eql?(apikey)
+    get "/ontologies", {}, {"Authorization" => 'apikey token="'+@apikey+'&userapikey='+@userapikey+'"'}
+    assert last_response.status == 200
+    apikey = MultiJson.load(last_response.body)
+    assert @userapikey.eql?(apikey)
+    get "/ontologies", {}, {"Authorization" => "apikey token=#{@apikey}&userapikey=#{@userapikey}"}
+    assert last_response.status == 200
+    apikey = MultiJson.load(last_response.body)
+    assert @userapikey.eql?(apikey)
+    get "/ontologies?apikey=#{@apikey}&userapikey=#{@userapikey}"
+    assert last_response.status == 200
+    apikey = MultiJson.load(last_response.body)
+    assert @userapikey.eql?(apikey)
   end
 
 end
