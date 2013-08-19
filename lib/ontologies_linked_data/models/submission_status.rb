bd5ba386f9818b9fdccfacffbc19e5ce12771126
Paul R Alexander <palexander@stanford.edu>
2013-08-19 10:03:16 -0700

    Add archive to submission status

--- lib/ontologies_linked_data/models/submission_status.rb
+++ lib/ontologies_linked_data/models/submission_status.rb
@@ -1,7 +1,7 @@
 module LinkedData
   module Models
     class SubmissionStatus < LinkedData::Models::Base
-      VALUES = ["UPLOADED", "RDF", "LABELS", "INDEXED", "READY", "ERROR_LABELS","ERROR_RDF", "ERROR_INDEX"]
+      VALUES = ["UPLOADED", "RDF", "LABELS", "INDEXED", "READY", "ERROR_LABELS", "ERROR_RDF", "ERROR_INDEX", "ARCHIVED"]
       model :submission_status, name_with: :code
       attribute :code, enforce: [:existence, :unique]
       attribute :submissions,
