cca36f9028d03c58c0b31a99602fe838fa5859b4
msalvadores <msalvadores@gmail.com>
2013-09-24 11:28:53 -0700

    no need to clean redis in test mappings

--- test/models/test_mappings.rb
+++ test/models/test_mappings.rb
@@ -9,19 +9,6 @@ class TestMapping < LinkedData::TestOntologyCommon
       raise Exception, "KB with too many mappings to run test. Is this pointing to a TEST KB?"
     end
 
-    @@redis = Redis.new(:host => LinkedData.settings.redis_host, 
-                        :port => LinkedData.settings.redis_port)
-    db_size = @@redis.dbsize
-    if db_size > 2000
-      puts "   This test cannot be run. You are probably pointing to the wrong redis backend. "
-      return
-    end
-
-    mappings = @@redis.keys.select { |x| x["mappings:"] }
-    if mappings.length > 0
-      @@redis.del(mappings)
-    end
-
     LinkedData::Models::MappingProcess.all do |m|
       m.delete
     end
