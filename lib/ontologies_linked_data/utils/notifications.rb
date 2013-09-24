06e7d903b7b5bcf4e8caad7854c1f8beb28ab7a3
Paul R Alexander <palexander@stanford.edu>
2013-09-23 22:33:49 -0700

    New note notification

--- lib/ontologies_linked_data/utils/notifications.rb
+++ lib/ontologies_linked_data/utils/notifications.rb
@@ -24,6 +24,35 @@ module LinkedData::Utils
       })
     end
 
+    def self.new_note(note)
+      note.bring_remaining
+      note.creator.bring(:username) if note.creator.bring?(:username)
+      note.relatedOntology.each {|o| o.bring(:name) if o.bring?(:name); o.bring(:subscriptions) if o.bring?(:subscriptions)}
+      subject = "[BioPortal Notes] #{note.subject}"
+      body = NEW_NOTE.gsub("%username%", note.creator.username)
+                     .gsub("%ontologies%", note.relatedOntology.map {|o| o.name}.join(", "))
+                     .gsub("%note_url%", LinkedData::Hypermedia.generate_links(note)["ui"])
+                     .gsub("%note_subject%", note.subject || "")
+                     .gsub("%note_body%", note.body || "")
+
+
+      emails = []
+      note.relatedOntology.each do |ont|
+        ont.subscriptions.each do |subscription|
+          subscription.bring(:notification_type) if subscription.bring?(:notification_type)
+          subscription.notification_type.bring(:type) if subscription.notification_type.bring?(:notification_type)
+          next unless subscription.notification_type.type.eql?("NOTES")
+          subscription.bring(:user) if subscription.bring?(:user)
+          subscription.user.each do |user|
+            user.bring(:email) if user.bring?(:email)
+            emails << notify(recipient: user.email, subject: subject, body: body)
+          end
+        end
+      end
+
+      emails
+    end
+
     private
 
     def self.mail_options
@@ -43,5 +72,18 @@ module LinkedData::Utils
 
       return options
     end
+
+NEW_NOTE = <<EOS
+A new note was added to %ontologies% by <b>%username%</b>.<br/><br/>
+
+----------------------------------------------------------------------------------<br/>
+<b>Subject:</b> %note_subject%<br/>
+
+%note_body%
+----------------------------------------------------------------------------------<br/><br/>
+
+You can respond by visiting: <a href="%note_url%">NCBO BioPortal</a>.<br/><br/>
+EOS
+
   end
 end
