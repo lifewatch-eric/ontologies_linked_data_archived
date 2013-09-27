462827b1599bafa53a037953d037bd03031832f2
Paul R Alexander <palexander@stanford.edu>
2013-09-27 14:26:05 -0700

    Human-readable status

--- lib/ontologies_linked_data/models/submission_status.rb
+++ lib/ontologies_linked_data/models/submission_status.rb
@@ -11,6 +11,22 @@ module LinkedData
         "ARCHIVED", "ERROR_ARCHIVED"
       ]
 
+      USER_READABLE = {
+        "RDF"             => "Parsed successfully",
+        "RDF_ERROR"       => "Error parsing",
+        "INDEXED"         => "Indexed for search",
+        "ERROR_INDEXED"   => "Error indexing for search",
+        "METRICS"         => "Class metrics calculated",
+        "ERROR_METRICS"   => "Error calculating class metrics",
+        "ANNOTATOR"       => "Processed for use in Annotator",
+        "ERROR_ANNOTATOR" => "Error processing for use in Annotator"
+      }
+
+      USER_IGNORE = [
+        "UPLOADED", "ERROR_UPLOADED",
+        "RDF_LABELS", "ERROR_RDF_LABELS",
+      ]
+
       model :submission_status, name_with: :code
       attribute :code, enforce: [:existence, :unique]
       attribute :submissions,
@@ -38,6 +54,13 @@ module LinkedData
 
       def get_code_from_id
         return self.id.to_s.split("/")[-1]
+      end
+
+      def self.readable_statuses(statuses)
+        statuses = statuses.map {|s| s.code}
+        statuses = statuses - USER_IGNORE
+        statuses.sort! {|a,b| VALUES.index(a) <=> VALUES.index(b)}
+        statuses.map {|s| USER_READABLE[s] || s.capitalize}
       end
 
       def self.status_ready?(status)
