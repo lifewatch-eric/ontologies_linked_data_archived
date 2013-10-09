a2e44da03bf50022598fb32dd69a81815ccd89c0
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-10-09 11:59:22 -0700

    Revise config tests

--- test/util/test_config.rb
+++ test/util/test_config.rb
@@ -3,43 +3,48 @@ require_relative "../../lib/ontologies_linked_data"
 
 class TestLinkedDataConfig < MiniTest::Unit::TestCase
 
+  def setup
+    Goo.class_variable_set("@@sparql_backends", {})
+    LinkedData.instance_variable_set("@settings_run", false)
+    LinkedData.settings.goo_host = nil
+    LinkedData.settings.goo_port = nil
+  end
+
   def teardown
     Goo.class_variable_set("@@sparql_backends", {})
     LinkedData.instance_variable_set("@settings_run", false)
-    load(File.expand_path("../../../config/config.rb", __FILE__))
   end
 
   def test_default_config
-    Goo.class_variable_set("@@sparql_backends", {})
-    LinkedData.instance_variable_set("@settings_run", false)
+    puts
     LinkedData.config()
-    assert_equal(LinkedData.settings.goo_host, 'localhost', msg="goo_host != localhost")
-    assert_equal(Goo.sparql_data_client.nil?, false, msg='sparql_data_client is nil')
-    assert_equal(Goo.sparql_update_client.nil?, false, msg='sparql_update_client is nil')
-    assert_equal(Goo.sparql_query_client.nil?, false, msg='sparql_query_client is nil')
+    assert_equal('localhost', LinkedData.settings.goo_host, msg="goo_host != localhost")
+    assert_equal(9000, LinkedData.settings.goo_port, msg="goo_port != 9000")
+    assert_equal(false, Goo.sparql_data_client.nil?, msg='sparql_data_client is nil')
+    assert_equal(false, Goo.sparql_update_client.nil?, msg='sparql_update_client is nil')
+    assert_equal(false, Goo.sparql_query_client.nil?, msg='sparql_query_client is nil')
   end
 
   def test_custom_config
+    puts
+    test_host = 'test_host'
     test_port = 1111
-    test_host = "test_host"
-
-    # Override safety check
-    LinkedData.instance_variable_set("@settings_run", false)
-
     # Re-configure
     LinkedData.config do |config, overide_connect_goo|
-      # Prevent goo connection
-      overide_connect_goo = true
-      # Settings
+      overide_connect_goo = true # Prevent goo connection
       config.goo_port = test_port
       config.goo_host = test_host
     end
+    assert_equal(test_host, LinkedData.settings.goo_host, msg="goo_host != test_host")
+    assert_equal(test_port, LinkedData.settings.goo_port, msg="goo_port != test_port")
+    assert_equal(false, Goo.sparql_data_client.nil?, msg='sparql_data_client is nil')
+    assert_equal(false, Goo.sparql_update_client.nil?, msg='sparql_update_client is nil')
+    assert_equal(false, Goo.sparql_query_client.nil?, msg='sparql_query_client is nil')
+  end
 
-    assert_equal(LinkedData.settings.goo_host, test_host, msg="goo_host != test_host")
-    assert_equal(LinkedData.settings.goo_port, test_port, msg="goo_port != test_port")
-    assert_equal(Goo.sparql_data_client.nil?, false, msg='sparql_data_client is nil')
-    assert_equal(Goo.sparql_update_client.nil?, false, msg='sparql_update_client is nil')
-    assert_equal(Goo.sparql_query_client.nil?, false, msg='sparql_query_client is nil')
+  def test_config_file
+    puts
+    load(File.expand_path("../../../config/config.rb", __FILE__))
   end
 
 end
