9e558b136fcc07be954de3c93bbcb1eb67c153bd
Darren L. Weber, Ph.D. <dweber.consulting@gmail.com>
2013-11-27 15:39:08 -0800

    Update test_case.rb
    Modify safe host regex, include ncbo-unittest*.

--- test/test_case.rb
+++ test/test_case.rb
@@ -17,7 +17,7 @@ require 'minitest/unit'
 MiniTest::Unit.autorun
 
 # Check to make sure you want to run if not pointed at localhost
-safe_hosts = Regexp.new(/localhost|ncbo-dev*|ncbo-stg-app-22*/)
+safe_hosts = Regexp.new(/localhost|ncbo-dev*|ncbo-unittest*/)
 def safe_redis_hosts?(sh)
   return [LinkedData.settings.http_redis_host,
    LinkedData.settings.goo_redis_host].select { |x|
