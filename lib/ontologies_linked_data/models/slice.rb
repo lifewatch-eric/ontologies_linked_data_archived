1da0bbe35c0a5e530fddf12e4f5dd1ed5d93e75e
Paul R Alexander <palexander@stanford.edu>
2013-11-26 16:15:03 -0800

    Slice model

---
+++ lib/ontologies_linked_data/models/slice.rb
@@ -0,0 +1,46 @@
+require 'ontologies_linked_data/models/group'
+
+module LinkedData::Models
+  class Slice < LinkedData::Models::Base
+    model :slice, name_with: :acronym
+    attribute :acronym, enforce: [:unique, :existence, lambda {|inst,attr| validate_acronym(inst, attr)}]
+    attribute :name, enforce: [:existence]
+    attribute :description
+    attribute :created, enforce: [:date_time], :default => lambda { |record| DateTime.now }
+    attribute :ontologies, enforce: [:existence, :list, :ontology]
+
+    def self.validate_acronym(inst, attr)
+      inst.bring(attr) if inst.bring?(attr)
+      value = inst.send(attr)
+      acronym_regex = /\A[-_a-z]+\Z/
+      if (acronym_regex.match value).nil?
+        return [:acronym_value_validator,"The acronym value #{value} is invalid"]
+      end
+      return [:acronym_value_validator, nil]
+    end
+
+    def self.synchronize_groups_to_slices
+      # Check to make sure each group has a corresponding slice (and ontologies match)
+      groups = LinkedData::Models::Group.where.include(LinkedData::Models::Group.attributes(:all)).all
+      groups.each do |g|
+        slice = self.find(g.acronym).first
+        if slice
+          slice.ontologies = g.ontologies
+          slice.save if slice.valid?
+        else
+          slice = self.new({
+            acronym: g.acronym.downcase.gsub(" ", "_"),
+            name: g.name,
+            description: g.description,
+            ontologies: g.ontologies
+          })
+          slice.save rescue binding.pry
+        end
+      end
+    end
+
+    def ontology_id_set
+      @ontology_set ||= Set.new(self.ontologies.map {|o| o.id.to_s})
+    end
+  end
+end
