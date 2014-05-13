e74128a627000fc84bcbe0a1f484569d262dbccf
msalvadores <msalvadores@gmail.com>
2014-05-13 08:30:09 -0700

    correct Obo in OWL ns for xref mappings

--- test/models/test_mappings.rb
+++ test/models/test_mappings.rb
@@ -496,7 +496,6 @@ class TestMapping < LinkedData::TestOntologyCommon
                                  .include(process: [:name])
                                  .all
 
-    binding.pry
     assert LinkedData::Models::TermMapping.where.all.length == 4
     assert mappings.length == 2
     mappings.each do |map|
