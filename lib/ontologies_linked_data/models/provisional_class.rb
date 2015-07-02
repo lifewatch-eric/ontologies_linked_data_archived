f5eef40899e9e34a0ffcb95fb64cae284df877c1
mdorf <mdorf@stanford.edu>
2015-07-01 17:11:43 -0700

    implementing NCBO-1416 - Allow search of provisional classes with a specified ontology

--- lib/ontologies_linked_data/models/provisional_class.rb
+++ lib/ontologies_linked_data/models/provisional_class.rb
@@ -11,7 +11,72 @@ module LinkedData
       attribute :created, enforce: [:date_time], :default => lambda { |record| DateTime.now }
       attribute :permanentId, enforce: [:uri]
       attribute :noteId, enforce: [:uri]
-      attribute :ontology, enforce: [:list, :ontology]
+      attribute :ontology, enforce: [:ontology]
+
+      search_options :index_id => lambda { |t| t.index_id },
+                     :document => lambda { |t| t.index_doc }
+
+
+      def index_id
+        return nil unless self.ontology
+        latest = self.ontology.latest_submission(status: :any)
+        return nil unless latest
+        "#{self.id.to_s}_#{self.ontology.acronym}_#{latest.submissionId}"
+      end
+
+      def index_doc
+        return {} unless self.ontology
+        latest = self.ontology.latest_submission(status: :any)
+        return {} unless latest
+
+        doc = {
+          :resource_id => self.id.to_s,
+          :prefLabel => self.label,
+          :obsolete => false,
+          :provisional => true,
+          :submissionAcronym => self.ontology.acronym,
+          :submissionId => latest.submissionId
+        }
+
+        all_attrs = self.to_hash
+        std = [:id, :synonym, :definition]
+
+        std.each do |att|
+          cur_val = all_attrs[att]
+          # don't store empty values
+          next if cur_val.nil? || cur_val.empty?
+
+          if (cur_val.is_a?(Array))
+            doc[att] = []
+            cur_val = cur_val.uniq
+            cur_val.map { |val| doc[att] << (val.kind_of?(Goo::Base::Resource) ? val.id.to_s : val.to_s.strip) }
+          else
+            doc[att] = cur_val.to_s.strip
+          end
+        end
+
+        doc
+      end
+
+      def index
+        if index_id
+          unindex
+          super
+          LinkedData::Models::Ontology.indexCommit
+        end
+      end
+
+      def unindex
+        if index_id
+          query = "id:#{solr_escape(index_id)}"
+          LinkedData::Models::Ontology.unindexByQuery(query)
+          LinkedData::Models::Ontology.indexCommit
+        end
+      end
+
+      def solr_escape(text)
+        RSolr.solr_escape(text).gsub(/\s+/,"\\ ")
+      end
     end
   end
 end
