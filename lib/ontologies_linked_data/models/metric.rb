91940c69a43578bb07f9ca9ef059773cb3108160
mdorf <mdorf@stanford.edu>
2018-01-29 12:42:47 -0800

    resolved an issue with retrieving all ontology submissions

--- lib/ontologies_linked_data/models/metric.rb
+++ lib/ontologies_linked_data/models/metric.rb
@@ -22,8 +22,30 @@ module LinkedData
 
       # Hypermedia links
       links_load submission: [:submissionId, ontology: [:acronym]]
-      link_to LinkedData::Hypermedia::Link.new("ontology", lambda {|m| "ontologies/#{m.submission.first.ontology.acronym}"}, Goo.vocabulary["Ontology"]),
-              LinkedData::Hypermedia::Link.new("submission", lambda {|m| "ontologies/#{m.submission.first.ontology.acronym}/submissions/#{m.submission.first.submissionId}"}, Goo.vocabulary["OntologySubmission"])
+      link_to LinkedData::Hypermedia::Link.new("ontology", lambda {|m| "#{self.ontology_submission_links(m)[:ont]}"}, Goo.vocabulary["Ontology"]),
+              LinkedData::Hypermedia::Link.new("submission", lambda {|m| "#{self.ontology_submission_links(m)[:ont]}#{ontology_submission_links(m)[:sub]}"}, Goo.vocabulary["OntologySubmission"])
+
+      def self.ontology_submission_links(m)
+        acronym_link = ""
+        submission_link = ""
+
+        if m.class == self
+          m.bring(:submission) if m.bring?(:submission)
+
+          begin
+            m.submission.first.bring(:ontology) if m.submission.first.bring?(:ontology)
+            ont = m.submission.first.ontology
+            ont.bring(:acronym) if ont.bring?(:acronym)
+            acronym_link = "ontologies/#{ont.acronym}"
+            submission_link = "/submissions/#{m.submission.first.submissionId}"
+          rescue Exception => e
+            acronym_link = ""
+            submission_link = ""
+          end
+        end
+
+        {ont: acronym_link, sub: submission_link}
+      end
 
       def self.metrics_id_generator(m)
         raise ArgumentError, "Metrics id needs to be set"
