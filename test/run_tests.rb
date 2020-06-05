3fbdbaa0a4c055128e53995b7d9acff0b58501aa
mdorf <mdorf@stanford.edu>
2020-06-05 12:57:17 -0700

    added ability to dockerize 4store - #105

--- test/run_tests.rb
+++ test/run_tests.rb
@@ -8,68 +8,86 @@ BACKEND_4STORE = '4store'
 GOO_HOST = 'localhost'
 AG_USERNAME = 'test'
 AG_PASSWORD = 'xyzzy'
-AG_PORT = 10035
-AG_HOST = "http://#{GOO_HOST}:#{AG_PORT}"
+DEF_AG_PORT = 10035
+DEF_4STORE_PORT = 9000
 JOB_NAME = 'bioportal'
+DEF_VERSION = 'latest'
 @options = nil
 
 def main
   @options = parse_options
 
   if @options[:backend] == BACKEND_AG
-    puts "\nUsing AllegroGraph v#{@options[:version]} on port #{AG_PORT}...\n\n"
-    pull_cmd = "docker pull franzinc/agraph:v#{@options[:version]}"
-    run_cmd = "docker run -d -e AGRAPH_SUPER_USER=#{AG_USERNAME} -e AGRAPH_SUPER_PASSWORD=#{AG_PASSWORD} -p #{AG_PORT}:#{AG_PORT} --shm-size 1g --name agraph-#{@options[:version]}-#{AG_PORT} franzinc/agraph:v#{@options[:version]}"
+    @options[:port] = DEF_AG_PORT if @options[:port] <= 0
+    puts "\nUsing AllegroGraph #{@options[:version]} on port #{@options[:port]}...\n\n"
+    pull_cmd = "docker pull franzinc/agraph:#{@options[:version]}"
+    run_cmd = "docker run -d -e AGRAPH_SUPER_USER=#{AG_USERNAME} -e AGRAPH_SUPER_PASSWORD=#{AG_PASSWORD} -p #{@options[:port]}:#{@options[:port]} --shm-size 1g --name #{@options[:backend]}-#{@options[:version]}-#{@options[:port]} franzinc/agraph:#{@options[:version]}"
     system("#{pull_cmd}")
     system("#{run_cmd}")
     sleep(5)
-    rest_call("/repositories/#{JOB_NAME}", 'PUT')
-    rest_call('/users/anonymous', 'PUT')
-    rest_call("/users/anonymous/access?read=true&write=true&repositories=#{JOB_NAME}", 'PUT')
+    ag_rest_call("/repositories/#{JOB_NAME}", 'PUT')
+    ag_rest_call('/users/anonymous', 'PUT')
+    ag_rest_call("/users/anonymous/access?read=true&write=true&repositories=#{JOB_NAME}", 'PUT')
   elsif @options[:backend] == BACKEND_4STORE
-    puts "\nUsing 4store...\n"
+    @options[:port] = DEF_4STORE_PORT if @options[:port] <= 0
+    puts "\nUsing 4store #{@options[:version]} on port #{@options[:port]}...\n\n"
+    pull_cmd = "docker pull bde2020/4store:#{@options[:version]}"
+    run_cmd = "docker run -d -p #{@options[:port]}:#{@options[:port]} --shm-size 1g --name #{@options[:backend]}-#{@options[:version]}-#{@options[:port]} bde2020/4store:#{@options[:version]}"
+    exec_cmd1 = "docker exec #{@options[:backend]}-#{@options[:version]}-#{@options[:port]} 4s-backend-setup --segments 4 demo"
+    exec_cmd2 = "docker exec #{@options[:backend]}-#{@options[:version]}-#{@options[:port]} 4s-admin start-stores demo"
+    exec_cmd3 = "docker exec #{@options[:backend]}-#{@options[:version]}-#{@options[:port]} 4s-httpd -s-1 -p#{@options[:port]} demo"
+    system("#{pull_cmd}")
+    system("#{run_cmd}")
+    system("#{exec_cmd1}")
+    system("#{exec_cmd2}")
+    sleep(5)
+    system("#{exec_cmd3}")
+    sleep(1)
   end
   test_cmd = 'bundle exec rake test'
   test_cmd << " TEST=\"#{@options[:filename]}\"" unless @options[:filename].empty?
-  test_cmd << " TESTOPTS=\"--name=#{@options[:test]}\"" unless @options[:test].empty?
+  test_cmd << ' TESTOPTS="-v"'
+  test_cmd.gsub!(/"$/, " --name=#{@options[:test]}\"") unless @options[:test].empty?
   puts "\n#{test_cmd}\n\n"
 
-  ENV['OVERRIDE_CONNECT_GOO'] = 'true'
-  ENV['GOO_HOST'] = GOO_HOST
-  ENV['GOO_BACKEND_NAME'] = @options[:backend]
+  begin
+    ENV['OVERRIDE_CONNECT_GOO'] = 'true'
+    ENV['GOO_HOST'] = GOO_HOST
+    ENV['GOO_PORT'] = @options[:port].to_s
+    ENV['GOO_BACKEND_NAME'] = @options[:backend]
 
-  if @options[:backend] == BACKEND_AG
-    ENV['GOO_PORT'] = AG_PORT.to_s
-    ENV['GOO_PATH_QUERY'] = '/repositories/bioportal'
-    ENV['GOO_PATH_DATA'] = '/repositories/bioportal/statements'
-    ENV['GOO_PATH_UPDATE'] = '/repositories/bioportal/statements'
-  elsif @options[:backend] == BACKEND_4STORE
-    ENV['GOO_PORT'] = 8080.to_s
-    ENV['GOO_PATH_QUERY'] = '/sparql/'
-    ENV['GOO_PATH_DATA'] = '/data/'
-    ENV['GOO_PATH_UPDATE'] = '/update/'
+    if @options[:backend] == BACKEND_AG
+      ENV['GOO_PATH_QUERY'] = '/repositories/bioportal'
+      ENV['GOO_PATH_DATA'] = '/repositories/bioportal/statements'
+      ENV['GOO_PATH_UPDATE'] = '/repositories/bioportal/statements'
+    elsif @options[:backend] == BACKEND_4STORE
+      ENV['GOO_PATH_QUERY'] = '/sparql/'
+      ENV['GOO_PATH_DATA'] = '/data/'
+      ENV['GOO_PATH_UPDATE'] = '/update/'
+    end
+    system("#{test_cmd}")
+  rescue StandardError => e
+    msg = "Failed test run with exception:\n\n#{e.class}: #{e.message}\n"
+    puts msg
+    puts e.backtrace
   end
 
-  system("#{test_cmd}")
-
-  if @options[:backend] == BACKEND_AG
-    img_name = "agraph-#{@options[:version]}-#{AG_PORT}"
-    rm_cmd = "docker rm -f -v #{img_name}"
-    puts "\nRemoving Docker Image: #{img_name}\n\n"
-    %x(#{rm_cmd})
-  end
+  img_name = "#{@options[:backend]}-#{@options[:version]}-#{@options[:port]}"
+  rm_cmd = "docker rm -f -v #{img_name}"
+  puts "\nRemoving Docker Image: #{img_name}\n"
+  %x(#{rm_cmd})
 end
 
-def rest_call(path, method)
+def ag_rest_call(path, method)
   data = {}
   response = RestClient::Request.new(
       :method => method,
-      :url => "#{AG_HOST}#{path}",
+      :url => "http://#{GOO_HOST}:#{@options[:port]}#{path}",
       :user => AG_USERNAME,
       :password => AG_PASSWORD,
       :headers => { :accept => :json, :content_type => :json }
   ).execute
-  data = JSON.parse(response.to_str) if ['POST' 'PUT'].include?(method)
+  data = JSON.parse(response.to_str) if ['get', 'post'].include?(method.downcase)
   data
 end
 
@@ -77,19 +95,24 @@ def parse_options
   backends = [BACKEND_4STORE, BACKEND_AG]
   options = {
       backend: BACKEND_4STORE,
-      version: '',
+      version: DEF_VERSION,
       filename: '',
-      test: ''
+      test: '',
+      port: -1
   }
   opt_parser = OptionParser.new do |opts|
     opts.banner = "Usage: bundle exec ruby #{File.basename(__FILE__)} [options]"
 
-    opts.on('-b', "--backend [#{BACKEND_4STORE}|#{BACKEND_AG}]", "An optional backend name (default: #{BACKEND_4STORE})") do |bn|
+    opts.on('-b', "--backend [#{BACKEND_4STORE}|#{BACKEND_AG}]", "An optional backend name. Default: #{BACKEND_4STORE}") do |bn|
       options[:backend] = bn.strip.downcase if backends.include?(bn.downcase)
     end
 
-    opts.on('-v', '--version VERSION', "Version of AllegroGraph to test against. Required if '#{BACKEND_AG}' is specified as the --backend") do |ver|
+    opts.on('-v', '--version VERSION', "An optional version of the server to test against. Default: '#{DEF_VERSION}'\n\t\t\t\t\s\s\s\s\sMust be a valid image tag published on repositories:\n\t\t\t\t\thttps://hub.docker.com/r/bde2020/4store/tags for #{BACKEND_4STORE}\n\t\t\t\t\thttps://hub.docker.com/r/franzinc/agraph/tags for #{BACKEND_AG}") do |ver|
       options[:version] = ver.strip.downcase
+    end
+
+    opts.on('-p', '--port PORT', "An optional port number of the server to test against. Default: #{DEF_4STORE_PORT} for #{BACKEND_4STORE}, #{DEF_AG_PORT} for #{BACKEND_AG}\n\t\t\t\t\s\s\s\s\sMust be a valid integer value") do |port|
+      options[:port] = port.strip.to_i
     end
 
     opts.on('-f', '--file TEST_FILE_PATH', "An optional path to a test file to be run. Default: all test files") do |f|
@@ -107,8 +130,8 @@ def parse_options
   end
   opt_parser.parse!
 
-  if options[:backend] == BACKEND_AG && options[:version].empty?
-    puts 'When using AllegroGraph, the version number is required. Run this script with the -h parameter for more information.'
+  if options[:port] == 0
+    puts 'The port number must be a valid integer. Run this script with the -h parameter for more information.'
     abort("Aborting...\n")
   end
   options[:test] = '' if options[:filename].empty?
